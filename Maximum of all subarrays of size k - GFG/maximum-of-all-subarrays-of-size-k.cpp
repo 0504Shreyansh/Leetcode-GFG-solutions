72ce8746f5a122d2dab9b44331ec0eea1db9cc9d
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-01-13 17:50:47 +0530

    Added solution - LeetHub

--- Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
@@ -18,18 +18,12 @@ class Solution
         vector<int> ans;
         int i = 0, j = 0;
         while(j < n) {
-            if(dq.empty())
-                dq.push_back(arr[j]);
-            else if(dq.back()>arr[j])
-                dq.push_back(arr[j]);
-            else {
-                while(dq.size() && dq.back()<arr[j])
-                    dq.pop_back();
-                dq.push_back(arr[j]);
-            }
+            while(dq.size() && dq.back()<arr[j])
+                dq.pop_back();
+            dq.push_back(arr[j]);
             if(j-i+1<k)
                 j++;
-            else if(j-i+1==k) {
+            else {
                 ans.push_back(dq.front());
                 if(dq.front()==arr[i])
                     dq.pop_front();
