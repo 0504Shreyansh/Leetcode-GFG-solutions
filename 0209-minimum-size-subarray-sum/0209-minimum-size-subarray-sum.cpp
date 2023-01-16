1d873379be8103a4dcd357c3bad0854501d3e71e
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-01-16 16:52:13 +0530

    Time: 27 ms (99.51%), Space: 28.3 MB (56.72%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+/*1. Almost similar to maximum size subarray of sum k.
+  2. Only change is to apply atleast sum k.
+  3. Apply variable size sliding window concept.
+*/
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+        int i = 0, j = 0, ans = INT_MAX;
+        int sum = 0;
+        while(j<nums.size()) {
+            sum += nums[j];
+            if(sum<target)
+                j++;
+            else {
+                while(sum>=target) {
+                    ans = min(ans, j-i+1);
+                    sum -= nums[i++];
+                } j++;
+            }
+            // cout << i << ' ' << j << ' ' << sum << ' ' << ans << endl;
+        }
+        
+        return (ans==INT_MAX) ? 0 : ans;
+    }
+};
