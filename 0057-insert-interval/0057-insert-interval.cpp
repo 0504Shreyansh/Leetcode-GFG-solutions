47055cb166635a34d4c02d0bd7dc1b1b39e2b106
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-01-16 17:35:00 +0530

    Time: 40 ms (22.22%), Space: 17.7 MB (16.34%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,4 +1,12 @@
 class Solution {
+/*1. Insert the given interval and sort to obtain the desired position of the interval{s,e}.
+  2. Move forward and keep checking for overlapping intervals
+     (e>=nextInterval[0])
+  3. Move backward and keep checking for overlapping intervals
+     (s<=nextInterval[1])
+  4. While u encounter another interval that needs to be merged, replace it with {-1,-1}.
+  5. Update both s and e to min(s,interval[0]) and max(e,,interval[1]).
+*/
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         
@@ -8,6 +16,7 @@ public:
         int in = find(intervals.begin(),intervals.end(),newInterval) - intervals.begin();
         int s = newInterval[0], e = newInterval[1];
         int j = in+1, i = in-1;
+        //Forward check
         while(j<n) {
             if(e>=intervals[j][0]) {
                 intervals[in][1] = max(intervals[in][1], intervals[j][1]);
@@ -17,6 +26,7 @@ public:
             }
             else break;
         }
+        //Backward check
         while(i>=0) {
             if(s<=intervals[i][1]) {
                 intervals[in][0] = min(intervals[in][0], intervals[i][0]);
@@ -28,6 +38,7 @@ public:
         }
         vector<vector<int>> ans;
         for(auto i:intervals) {
+            //Take intervals other than {-1,-1}
             if(i[0]!=-1 && i[1]!=-1)
                 ans.push_back(i);
         }
