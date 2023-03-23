91850c5f513e27fb27734e6241af56d12f58513e
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-03-23 14:25:32 +0530

    Time: 134 ms (69.17%), Space: 42.4 MB (71.87%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,32 +1,43 @@
-class Solution {
-public:
-    void dfs(int node, vector<int> adjList[], int n, vector<int> &vis) {
-        vis[node] = 1;
-        for(auto adjNode: adjList[node]) {
-            if(!vis[adjNode])
-                dfs(adjNode, adjList, n, vis);
-        }
+class DisjointSet {
+    public: 
+    vector<int> rank, parent;
+    DisjointSet(int n) {
+        rank.resize(n+1);
+        parent.resize(n+1);
+        for(int i = 0; i <= n; i++) 
+            parent[i] = i;
     }
-    int makeConnected(int numberOfNodes, vector<vector<int>>& connections) {
-        
-        //Create adjList
-        int numberOfEdges = connections.size();
-        int cntOfDisconnectedComps = 0;
-        vector<int> adjList[numberOfNodes], vis(numberOfNodes);
-        
-        for(auto edge: connections) {
-            adjList[edge[0]].push_back(edge[1]);
-            adjList[edge[1]].push_back(edge[0]);
-        }
-        
-        for(int node = 0; node < numberOfNodes; node++) {
-            if(!vis[node]) {
-                cntOfDisconnectedComps++;
-                dfs(node, adjList, numberOfNodes, vis);
+    int findParent(int node) {
+        return (node==parent[node]) ? node : parent[node]=findParent(parent[node]);
+    }
+    void unite(int u, int v) {
+        int up = findParent(u);
+        int vp = findParent(v);
+        if(up != vp) {
+            if(rank[up] < rank[vp]) 
+                parent[up] = vp;
+            else if(rank[up] > rank[vp])
+                parent[vp] = up;
+            else {
+                parent[up] = vp;
+                rank[vp]++;
             }
         }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        int res = 0;
+        DisjointSet ds(n);
+        for(auto it : connections) 
+            if(ds.findParent(it[0]) != ds.findParent(it[1])) 
+                ds.unite(it[0], it[1]);
+    
+        for(int i = 0; i < n; i++) 
+            res += (ds.parent[i]==i);
         
-        return (numberOfNodes-1 > numberOfEdges) ? -1 : (cntOfDisconnectedComps-1);
-        
+        return (connections.size() >= n-1) ? res-1 : -1;
     }
 };
