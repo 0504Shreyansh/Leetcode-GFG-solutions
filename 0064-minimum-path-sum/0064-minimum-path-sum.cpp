410b8c87d491bd82f3573b973a4eb0683399197f
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-03-27 20:03:02 +0530

    Time: 14 ms (24.22%), Space: 9.6 MB (98.74%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,22 +1,45 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n+1, vector<int> (m+1));
-        dp[0][0] = grid[0][0];
-        
-        for(int i = 1; i < n; i++) 
-            dp[i][0] += dp[i-1][0] + grid[i][0];
+    /*METHOD 1 : RECURISION
+    int recSol(int n, int m, int i, int j, vector<vector<int>>& grid) {
+        if(i<0 || j<0 || i>=n || j>=m) return 1e9;
+        if(i==0 && j==0) return grid[0][0];
+        return grid[i][j] + min(recSol(n,m,i-1,j,grid), recSol(n,m,i,j-1,grid));
+    }*/
+    /*  METHOD 2 : MEMOIZATION
+    int memoizedSol(int n, int m, int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if(i<0 || j<0 || i>=n || j>=m) return 1e9;
+        if(i==0 && j==0) return grid[0][0];
+        if(dp[i][j] != -1) return dp[i][j]l
+        return dp[i][j] = grid[i][j] + min(memoizedSol(n,m,i-1,j,grid,dp), memoizedSol(n,m,i,j-1,grid,dp));
+    }*/
+    /* METHOD 3 : TABULATION
+    int tabulatedSol(int n, int m, vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid);
+        for(int i = 1; i < n; i++)
+            dp[i][0] += dp[i-1][0];
         for(int j = 1; j < m; j++)
-            dp[0][j] += dp[0][j-1] + grid[0][j];
+            dp[0][j] += dp[0][j-1];
+        for(int i = 1; i < n; i++) 
+            for(int j = 1; j < m; j++) 
+                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);        
+        return dp[n-1][m-1];
+    }
+    */
+    int minPathSum(vector<vector<int>>& grid) {
+        // Space optimised
+        int n = grid.size(), m = grid[0].size();
+        vector<int> cur(m+1, 0), prev(grid[0]);
+        for(int j = 1; j < m; j++)
+            prev[j] += prev[j-1];
         
         for(int i = 1; i < n; i++) {
+            cur[0] = grid[i][0] + prev[0];
             for(int j = 1; j < m; j++) {
-                dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+                cur[j] = grid[i][j] + min(prev[j], cur[j-1]);
             }
+            prev = cur;
         }
-        
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
