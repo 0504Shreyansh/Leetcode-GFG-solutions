d2cf3799b5e444bea6aaaf557d84efefb01082d2
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-03-31 18:38:43 +0530

    Time: 4 ms (72.71%), Space: 7.3 MB (22.39%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,32 +1,70 @@
-// IT IS ALMOST SAME AS LEETCODE 44. WILDCARD MATCHING
 class Solution {
 public:
-    bool isMatchRecurs(string& s, string &p, int i, int j, vector<vector<int>> &dp) {
+    bool allStars(string s, int i) {
+        // Checks whether only stars are present or not
+        for(int k = 0; k <= i; k++)
+            if(s[k] != '*')
+                return false;
+        return true;
+    }
+    int recSol(int i, int j, string &pattern, string &str) {
         
-        if (j == p.size() && i == s.size()) 
+        // Both the strings are exhausted simultaneously
+        // i.e. they have been completely matched.
+        if(i<0 && j<0) 
             return true;
         
-        if (j == p.size())
+        // Pattern string is exhausted but 
+        // second string remains, so return false
+        if(i<0 && j>=0)
             return false;
-
-        if (dp[i][j] != -1) 
-            return dp[i][j];
-
-        bool firstMatch = i < s.size() && (s[i] == p[j] || p[j] == '.');
-        bool ans;
-        if (j+1 < p.size() && p[j+1] == '*') {
-            // Take or leave
-            ans = isMatchRecurs(s, p, i, j+2, dp) || firstMatch && isMatchRecurs(s, p, i+1, j, dp);
-        }
-        else {
-            ans = firstMatch && isMatchRecurs(s, p, i+1, j+1, dp);
-        }
-        return dp[i][j] = ans;
+        
+        // Only possible case of matching in case of string exhaustion
+        // Pattern string contains all stars!
+        if(i>=0 && j<0)
+            return allStars(pattern, i);
+        
+        // If the chars match just reduce both the indices by 1 
+        if(pattern[i] == str[j] || pattern[i] == '?')
+            return recSol(i-1, j-1, pattern, str);
+        
+        // Either don't consider inplace of '*'
+        // Or consider a character
+        if(pattern[i] == '*')
+            return recSol(i-1, j, pattern, str) | recSol(i, j-1, pattern, str);
+        
+        // No matching possible :(
+        return false;
     }
-
-    bool isMatch(string s, string p) {
-        int n = s.size(), m = p.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return isMatchRecurs(s, p, 0, 0, dp);        
+    bool isMatch(string str, string pattern) {
+        
+        int n = pattern.size(), m = str.size();
+        
+        if(n==0 && m==0)
+            return true;
+        
+        vector<bool> cur(m+1, false), prev(m+1, false);
+        // return recSol(n-1, m-1, pattern, str);
+        // Base case 
+        prev[0] = true;
+        
+        for(int i = 1; i <= n; i++) {
+            cur[0] = allStars(pattern, i-1);
+            for(int j = 1; j <= m; j++) {
+                
+                if(pattern[i-1] == str[j-1] || pattern[i-1] == '?')
+                    cur[j] = prev[j-1];
+                
+                else if(pattern[i-1] == '*')
+                    cur[j] = prev[j] | cur[j-1];
+                
+                else 
+                    cur[j] = false;
+            }
+            
+            prev = cur;
+        }
+        
+        return cur[m];
     }
 };
