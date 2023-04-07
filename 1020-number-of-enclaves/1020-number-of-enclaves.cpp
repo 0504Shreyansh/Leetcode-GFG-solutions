a35e58e225a63797b2ee682d55c45f6ef11e4c74
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-04-07 16:12:55 +0530

    Time: 90 ms (23.93%), Space: 48.6 MB (7.41%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>> &grid, vector<vector<int>> &visited, int row, int col, int &numberOfEnclaves, bool toCount) {
-        visited[row][col] = 1;
-        if(toCount)
-            numberOfEnclaves++;
-        int dx[] = {-1,0,1,0}, dy[] = {0,-1,0,1};
-        int n = grid.size(), m = grid[0].size();
-        
-        for (int index = 0; index < 4; index++) {
-            int new_row = row + dx[index], new_col = col + dy[index];
-            if(new_row>=0 && new_row<n && new_col>=0 && new_col<m && !visited[new_row][new_col] && grid[new_row][new_col])
-                dfs(grid, visited, new_row, new_col, numberOfEnclaves, toCount);
+    vector<pair<int,int>> direction = {{-1,0},{1,0},{0,1},{0,-1}};
+    void dfs(int &cnt, int i, int j, vector<vector<int>>& grid, bool toCount) {
+        grid[i][j] = 0;
+        if(toCount==true)
+            cnt++;
+        int N = grid.size(), M = grid[0].size();
+        for(auto [x, y] : direction) {
+            int X = i + x, Y = j + y;
+            if(X>=0 && X<N && Y>=0 && Y<M && grid[X][Y]==1) {
+                dfs(cnt, X, Y, grid, toCount);
+            }
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
+        int N = grid.size();
+        int M = grid[0].size();
+        int cnt = 0;
         
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> visited(n,vector<int>(m));
-        int numberOfEnclaves = 0;
-        
-        //Visit all the boundary 1's
-        for (int row = 0; row < n; row++) {
-            if (!visited[row][0] && grid[row][0])
-                dfs(grid, visited, row, 0, numberOfEnclaves, 0);
-            if (!visited[row][m-1] && grid[row][m-1])
-                dfs(grid, visited, row, m-1, numberOfEnclaves, 0);
+        for(int i = 0; i < N; i++) {
+            if(grid[i][0]==1)
+                dfs(cnt, i, 0, grid, 0);
+            if(grid[i][M-1]==1)
+                dfs(cnt, i, M-1, grid, 0);
         }
         
-        for (int col = 0; col < m; col++) {
-            if (!visited[0][col] && grid[0][col])
-                dfs(grid, visited, 0, col, numberOfEnclaves, 0);
-            if (!visited[n-1][col] && grid[n-1][col])
-                dfs(grid, visited, n-1, col, numberOfEnclaves, 0);
+        for(int j = 0; j < M; j++) {
+            if(grid[0][j]==1)
+                dfs(cnt, 0, j, grid, 0);
+            if(grid[N-1][j]==1)
+                dfs(cnt, N-1, j, grid, 0);
         }
         
-//         //Then, count the number of enclaved 1's
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < m; col++) {
-                if (!visited[row][col] && grid[row][col])
-                    dfs(grid, visited, row, col, numberOfEnclaves, 1);
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(grid[i][j]==1) {
+                    dfs(cnt, i, j, grid, true);
+                }
             }
         }
-        
-        return numberOfEnclaves;
+        return cnt;
     }
 };
