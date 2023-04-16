fcde995f8a11567e78b59c3fa93806e42a279924
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-04-16 21:47:13 +0530

    Time: 287 ms (59.91%), Space: 35.4 MB (64.22%) - LeetHub

---
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -0,0 +1,25 @@
+#define mod 1000000007
+class Solution {
+public:
+    int numWays(vector<string>& words, string target) {
+        int n = words[0].size();
+        int m = target.size();
+        vector<int> dp(m+1, 0);
+        dp[0] = 1;
+        
+        vector<vector<int>> count(n, vector<int>(26, 0));
+        for(auto word : words) {
+            for(int i = 0; i < n; i++) {
+                count[i][word[i] - 'a']++;
+            }
+        }
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = m-1; j >= 0; j--) {
+                dp[j+1] = (dp[j+1] + (int)((long)dp[j] * count[i][target[j] - 'a'] % mod)) % mod;
+            }
+        }
+        
+        return dp[m];
+    }
+};
