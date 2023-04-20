29f24f74a0619f05f0af03744d8711addc2e7061
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-04-20 19:29:11 +0530

    Time: 20 ms (5.61%), Space: 17.4 MB (25.97%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -10,31 +10,25 @@
  * };
  */
 class Solution {
-/*Concept -> 
-1. Store nodes with their indices
-2. At a particular level, calculate the 
-*/
 public:
     int widthOfBinaryTree(TreeNode* root) {
+        if (!root) return 0;
         long ans = 0;
-        if(!root)
-            return ans;
         queue<pair<int,TreeNode*>> q;
         q.push({0,root});
-        
-        while(q.size()) {
+        while (q.size()) {
             int n = q.size();
             long l = q.front().first;
             long h = q.back().first;
             ans = max(ans, h-l+1);
-            for(int i=0;i<n;i++) {
+            for (int i = 0; i < n; i++) {
                 long ind = q.front().first;
                 TreeNode* p = q.front().second;
                 q.pop();
                 // cout<<ind<<' '<<l<<' '<<h<<' '<<p->val<<endl;
-                if(p->left)
+                if (p->left)
                     q.push({2*ind+1,p->left});
-                if(p->right)
+                if (p->right)
                     q.push({2*ind+2,p->right});
             }
         }
