7fbcbfab741b734a9b27fd467995c2400d552973
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-24 19:17:09 +0530

    Time: 272 ms (11.71%), Space: 58.3 MB (5.69%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    int solve(int i, int j, bool flag, vector<int>& A, vector<int>& B, vector<vector<vector<int>>>& dp) {
-        if(i >= A.size() || j >= B.size()) {
-            return flag ? 0 : -1e9;
-        }
-        if(dp[i][j][flag] != -1) {
-            return dp[i][j][flag];
-        }
-        int skip1 = solve(i + 1, j, flag, A, B, dp);
-        int skip2 = solve(i, j + 1, flag, A, B, dp);
-        int take = (A[i] * B[j]) + solve(i + 1, j + 1, true, A, B, dp);
-        return dp[i][j][flag] = max({skip1, skip2, take});
-    }
     int maxDotProduct(vector<int>& A, vector<int>& B) {
         int N = A.size();
         int M = B.size();
