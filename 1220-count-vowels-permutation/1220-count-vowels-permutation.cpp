fd8e53eed3a1dcbfc48843f0e168b42f8b67e02b
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-24 15:00:05 +0530

    Time: 72 ms (44.74%), Space: 23.9 MB (45.79%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,62 +1,22 @@
 #define mod 1000000007
 class Solution {
 public:
-    int solve(int n, char prev, vector<vector<int>>& dp) {
-        if(n == 0) {
-            return 1;
-        }
-        
-        if(dp[n][prev - 'a'] != -1) {
-            return dp[n][prev - 'a'];
-        }
-        
-        if(prev == 'z') {
-            int prevA = solve(n - 1, 'a', dp);
-            int prevE = solve(n - 1, 'e', dp);
-            int prevI = solve(n - 1, 'i', dp);
-            int prevO = solve(n - 1, 'o', dp);
-            int prevU = solve(n - 1, 'u', dp);
-            return ((((((prevA + prevE) % mod) + prevI) % mod + prevO) % mod + prevU) % mod) % mod;
-        }
-        
-        int cur = 0;
-        if(prev == 'a') {
-            int prevE = solve(n - 1, 'e', dp); 
-            cur = (cur + prevE) % mod;
-            return dp[n][prev - 'a'] = cur % mod;
-        }
-        
-        else if(prev == 'e') {
-            int prevA = solve(n - 1, 'a', dp);
-            int prevI = solve(n - 1, 'i', dp);
-            cur = (cur + prevA + prevI) % mod;
-            return dp[n][prev - 'a'] = cur % mod;
-        }
-        
-        else if(prev == 'i') {
-            int prevA = solve(n - 1, 'a', dp);
-            int prevE = solve(n - 1, 'e', dp);
-            int prevO = solve(n - 1, 'o', dp);
-            int prevU = solve(n - 1, 'u', dp); 
-            cur = (cur + ((((prevA + prevE) % mod) + prevO) % mod + prevU) % mod) % mod;
-            return dp[n][prev - 'a'] = cur % mod;
-        }
-        
-        else if(prev == 'o') {
-            int prevI = solve(n - 1, 'i', dp);
-            int prevU = solve(n - 1, 'u', dp); 
-            cur = (cur + prevI + prevU) % mod;
-            return dp[n][prev - 'a'] = cur % mod;
-        }
-        
-        else {
-            int prevA = solve(n - 1, 'a', dp);
-            cur = (cur + prevA) % mod;
-            return dp[n][prev - 'a'] = cur % mod;
-        }         
-    }
     int countVowelPermutation(int n) {
-        vector<vector<int>> dp(n + 1, vector<int> (26, -1));
-        return solve(n, 'z', dp);
+        vector<vector<int>> dp(n + 1, vector<int> (5, -1));
+        for(int i = 0; i < 5; i++) {
+            dp[1][i] = 1;
+        }
+        for(int i = 2; i <= n; i++) {
+            dp[i][0] = (dp[i - 1][1]) % mod;  //a
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;  //e
+            dp[i][2] = ((((dp[i - 1][0] + dp[i - 1][1]) % mod) + dp[i - 1][3]) % mod + dp[i - 1][4]) % mod;   //i
+            dp[i][3] = (dp[i - 1][2] + dp[i - 1][4]) % mod;  //o
+            dp[i][4] = (dp[i - 1][0]) % mod;  //u
+        }
+        int ans = 0;
+        // count all vowels
+        for(int i = 0; i < 5; i++) 
+            ans = (ans + dp[n][i]) % mod;
+        return ans;
     }
 };
