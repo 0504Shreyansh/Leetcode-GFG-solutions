1c39bd3c3c8391f514cb7428cca3f276cc4c4780
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-24 19:16:05 +0530

    Time: 250 ms (12.04%), Space: 58.2 MB (6.02%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -18,6 +18,26 @@ public:
         vector<vector<vector<int>>> dp(N + 1, 
             vector<vector<int>> (M + 1, vector<int> (2, -1)));
         
-        return solve(0, 0, false, A, B, dp);
+        // Base cases
+        for(int i = 0; i <= N; i++) {
+            dp[i][M][0] = -1e9;
+            dp[i][M][1] = 0;
+        }
+        for(int j = 0; j <= M; j++) {
+            dp[N][j][0] = -1e9;
+            dp[N][j][1] = 0;
+        }
+        
+        for(int i = N - 1; i >= 0; i--) {
+            for(int j = M - 1; j >= 0; j--) {
+                for(int flag = 1; flag >= 0; flag--) {
+                    int skip1 = dp[i + 1][j][flag];
+                    int skip2 = dp[i][j + 1][flag];
+                    int take = (A[i] * B[j]) + dp[i + 1][j + 1][1];
+                    dp[i][j][flag] = max({skip1, skip2, take});
+                }
+            }
+        }
+        return dp[0][0][0];
     }
 };
