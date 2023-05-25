703c990a9a62f1e8602d2d24fa45d1d18b155954
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-25 18:14:08 +0530

    Time: 737 ms (11.85%), Space: 106.6 MB (28.86%) - LeetHub

--- 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -32,8 +32,40 @@ public:
         // gcd of adjacent is 1
         // repetition at a gap of more than 1
         vector<vector<vector<int>>> dp(n + 1, 
-                vector<vector<int>> (7, vector<int> (7, -1)));
+                vector<vector<int>> (7, vector<int> (7, 0)));
         // prev2 -> prev ka prev, prev -> exact prev
-        return solve(n, 0, 0, dp);
+        
+        
+        // Base case
+        for(int prev = 0; prev <= 6; prev++) {
+            for(int prev2 = 0; prev2 <= 6; prev2++) {
+                dp[0][prev][prev2] = 1;
+            }
+        }
+        
+        for(int i = 1; i <= n; i++) {
+            for(int prev = 0; prev <= 6; prev++) {
+                for(int prev2 = 0; prev2 <= 6; prev2++) {
+                    int curr = 0;
+                    for(int roll = 1; roll <= 6; roll++) {
+                        if(prev == 0) {  // first pick
+                            curr = (curr + dp[i - 1][roll][prev]) % mod;
+                        }
+                        else if(prev2 == 0) {  // second pick
+                            if(prev != roll && __gcd(prev, roll) == 1) {
+                                curr = (curr + dp[i - 1][roll][prev]) % mod;
+                            }
+                        }
+                        else if(prev2 != roll && prev != roll && __gcd(prev, roll) == 1) {
+                            curr = (curr + dp[i - 1][roll][prev]) % mod;
+                        }
+                    }
+                    dp[i][prev][prev2] = curr;
+                }
+            }
+        }
+        
+        
+        return dp[n][0][0];
     }
 };
