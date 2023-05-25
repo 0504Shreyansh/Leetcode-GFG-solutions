fe53496a93c1f6fa1990311b33422a07f76295f4
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-25 18:23:48 +0530

    Time: 470 ms (32.99%), Space: 6.2 MB (90.72%) - LeetHub

--- 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
+++ 2318-number-of-distinct-roll-sequences/2318-number-of-distinct-roll-sequences.cpp
@@ -1,45 +1,18 @@
 #define mod 1000000007
 class Solution {
 public:
-    int solve(int n, int prev, int prev2, vector<vector<vector<int>>>& dp) {
-        if(n == 0) {
-            return 1;
-        }
-        
-        if(dp[n][prev][prev2] != -1) {
-            return dp[n][prev][prev2];
-        }
-        
-        int curr = 0;
-        for(int roll = 1; roll <= 6; roll++) {
-            if(prev == 0) {  // first pick
-                curr = (curr + solve(n - 1, roll, prev, dp)) % mod;
-            }
-            else if(prev2 == 0) {  // second pick
-                if(prev != roll && __gcd(prev, roll) == 1) {
-                    curr = (curr + solve(n - 1, roll, prev, dp)) % mod;
-                }
-            }
-            else if(prev2 != roll && prev != roll && __gcd(prev, roll) == 1) {
-                curr = (curr + solve(n - 1, roll, prev, dp)) % mod;
-            }
-        }
-        
-        return dp[n][prev][prev2] = curr;
-    }
     int distinctSequences(int n) {
         // 2 conditions
         // gcd of adjacent is 1
         // repetition at a gap of more than 1
-        vector<vector<vector<int>>> dp(n + 1, 
-                vector<vector<int>> (7, vector<int> (7, 0)));
         // prev2 -> prev ka prev, prev -> exact prev
-        
+        vector<vector<int>> dp(7, vector<int> (7, 0));
+        vector<vector<int>> cur(7, vector<int> (7, 0));
         
         // Base case
         for(int prev = 0; prev <= 6; prev++) {
             for(int prev2 = 0; prev2 <= 6; prev2++) {
-                dp[0][prev][prev2] = 1;
+                dp[prev][prev2] = 1;
             }
         }
         
@@ -49,23 +22,23 @@ public:
                     int curr = 0;
                     for(int roll = 1; roll <= 6; roll++) {
                         if(prev == 0) {  // first pick
-                            curr = (curr + dp[i - 1][roll][prev]) % mod;
+                            curr = (curr + dp[roll][prev]) % mod;
                         }
                         else if(prev2 == 0) {  // second pick
                             if(prev != roll && __gcd(prev, roll) == 1) {
-                                curr = (curr + dp[i - 1][roll][prev]) % mod;
+                                curr = (curr + dp[roll][prev]) % mod;
                             }
                         }
                         else if(prev2 != roll && prev != roll && __gcd(prev, roll) == 1) {
-                            curr = (curr + dp[i - 1][roll][prev]) % mod;
+                            curr = (curr + dp[roll][prev]) % mod;
                         }
                     }
-                    dp[i][prev][prev2] = curr;
+                    cur[prev][prev2] = curr;
                 }
             }
+            dp = cur;
         }
         
-        
-        return dp[n][0][0];
+        return dp[0][0];
     }
 };
