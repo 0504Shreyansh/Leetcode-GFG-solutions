c0f4c26e8d082149db6461be5ab089d99d994d0d
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-26 10:59:35 +0530

    Time: 144 ms (47.62%), Space: 7.1 MB (93.88%) - LeetHub

--- 1223-dice-roll-simulation/1223-dice-roll-simulation.cpp
+++ 1223-dice-roll-simulation/1223-dice-roll-simulation.cpp
@@ -1,36 +1,14 @@
 #define mod 1000000007
 class Solution {
 public:
-    int solve(int n, int prev, int times, vector<int>& rollMax, vector<vector<vector<int>>>& dp) {
-        if(n == 0) {
-            return 1;
-        }
-        if(dp[n][prev][times] != -1) {
-            return dp[n][prev][times];
-        }
-        int curr = 0;
-        for(int i = 0; i < 6; i++) {
-            if(i + 1 != prev) {  // if different outcome, accept it and keep track
-                curr = (curr + solve(n - 1, i + 1, 1, rollMax, dp)) % mod;
-            }
-            else {
-                if(times != rollMax[i]) {  // make sure prev turns do not exceed repeat limit
-                    curr = (curr + solve(n - 1, prev, times + 1, rollMax, dp)) % mod;
-                }
-            }
-        }
-        return dp[n][prev][times] = curr;
-    }
     int dieSimulator(int n, vector<int>& rollMax) {
-        vector<vector<vector<int>>> dp(n + 1, 
-                vector<vector<int>> (7, vector<int> (17, -1)));
-        // return solve(n, 0, 0, rollMax, dp);
-        
-        
+     
+        vector<vector<int>> dp(7, vector<int> (17, -1));
+        vector<vector<int>> next(7, vector<int> (17, -1));
         
         for(int prev = 0; prev <= 6; prev++) {
             for(int times = 0; times <= 15; times++) {
-                dp[0][prev][times] = 1;
+                dp[prev][times] = 1;
             }
         }
         
@@ -41,19 +19,20 @@ public:
                     int curr = 0;
                     for(int roll = 1; roll <= 6; roll++) {
                         if(roll != prev) { 
-                            curr = (curr + dp[i - 1][roll][1]) % mod;
+                            curr = (curr + dp[roll][1]) % mod;
                         }
                         else {
                             if(times != rollMax[roll - 1]) { 
-                                curr = (curr + dp[i - 1][prev][times + 1]) % mod;
+                                curr = (curr + dp[prev][times + 1]) % mod;
                             }
                         }
                     }
-                    dp[i][prev][times] = curr;
+                    next[prev][times] = curr;
                 }
             }
+            dp = next;
         }
         
-        return dp[n][0][0];
+        return dp[0][0];
     }
 };
