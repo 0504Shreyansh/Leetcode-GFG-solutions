38b7b2df37cdfc09f3a75b566bc193c57cb533ab
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-05-29 19:48:54 +0530

    Time: 55 ms (31.95%), Space: 8.4 MB (63.89%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,40 +1,19 @@
 #define mod 1000000007
 class Solution {
 public:
-    int dp[31][1001];
-    int solve(int n, int k, int target) {
-        if(n==0) {
-            return (target==0) ? 1 : 0;
-        }
-
-        if(dp[n][target] != -1) 
-            return dp[n][target];
-
-        int ans = 0;
-        for(int i = 1; i <= k; i++) {
-            if(target >= i) {
-                ans = (ans + solve(n-1, k, target-i)) % mod;
-            }
-        }
-
-        return dp[n][target] = ans;
-    }
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n + 1, vector<int> (target + 1, 0));
-    
-        // return solve(n, k, target);
-        
-       
-        dp[0][0] = 1;
-        
-        for(int i = 1; i <= n; i++) {
-            for(int t = 1; t <= target; t++) {
-                for(int x = 1; x <= k && t - x >= 0; x++) {
-                    dp[i][t] += dp[i - 1][t - x];
-                    dp[i][t] = dp[i][t] % mod;
+        vector<int> prev(target + 1);
+        prev[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            vector<int> curr(target + 1);
+            for (int j = 0; j <= target; j++) {
+                for (int x = 1; x <= k && j - x >= 0; x++) {
+                    curr[j] = (curr[j] + prev[j - x]) % mod;
                 }
             }
+            prev = curr;
         }
-        return dp[n][target];   
+        return prev[target];  
     }
 };
