7f58d7b6c741d91481d1214675842086f9565e87
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-06 22:05:44 +0530

    Time: 686 ms (6.18%), Space: 37.8 MB (25.77%) - LeetHub

---
+++ 2514-count-anagrams/2514-count-anagrams.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    const int mod = 1e9 + 7;
+    #define ll long long
+    ll fact[1000001];
+    // Calculates the factorial of numbers up to 1e5
+    void pre_compute() {
+        fact[0] = fact[1] = 1;
+        for(int i = 2; i < 1000001; i++)
+            fact[i] = (i * fact[i - 1]) % mod;
+    }
+    // Calculates a^b in log(b) time
+    ll binpow(ll a, ll b) {
+        ll ans = 1;
+        while(b) {
+            if(b & 1)
+                ans = (ans * a) % mod;
+            b >>= 1;
+            a = (a * a) % mod;
+        }
+        return ans;
+    }
+    int countAnagrams(string s) {
+        pre_compute();
+        stringstream ss(s);
+        int ans = 1;
+        vector<string> words;
+        while(ss >> s) {    // take out all the words
+            words.push_back(s);
+        }
+        for(auto &word : words) {
+            unordered_map<char,int> freq;
+            for(auto &it : word)
+                freq[it]++;
+            ll curr = fact[word.size()];
+            for(auto &it : freq) {
+                curr = (curr * binpow(fact[it.second], mod - 2)) % mod;
+            }
+            ans = (ans * curr) % mod;
+        }
+        return ans;
+    }
+};
+
+/*
+shreyansh --> 9! / (2! * 2!)    n = 9, repeat: s(2), h(2).
+
+a / b % mod--> a * b^(-1) % mod--> a * b^(-1) % mod --> a * power(b, mod - 2)
+*/
