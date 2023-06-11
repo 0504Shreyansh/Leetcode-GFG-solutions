241121118e3591c0de102b1b6425962c76bc21c2
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-11 15:58:32 +0530

    Time: 385 ms (72.36%), Space: 143.6 MB (70.19%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,23 +1,22 @@
 class SnapshotArray {
 public:
-    int snapCount = 0;
-    unordered_map<int,map<int,int>> data;
-    
+    int snap_count = 0;
+    unordered_map<int, map<int,int>> snaps;
     SnapshotArray(int length) {
-    
+        
     }
     
     void set(int index, int val) {
-        data[index][snapCount] = val;
+        snaps[index][snap_count] = val;
     }
     
     int snap() {
-        return snapCount++;
+        return snap_count++;
     }
     
     int get(int index, int snap_id) {
-        auto it = data[index].upper_bound(snap_id);
-        return it == begin(data[index]) ? 0 : prev(it)->second;
+        auto it = snaps[index].upper_bound(snap_id);
+        return it == begin(snaps[index]) ? 0 : prev(it)->second;
     }
 };
 
