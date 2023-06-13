a951a719b583fb5dc9e8fd1d1265462aefc1b900
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-13 08:16:45 +0530

    Added solution - LeetHub

--- k largest elements - GFG/k-largest-elements.cpp
+++ k largest elements - GFG/k-largest-elements.cpp
@@ -9,18 +9,17 @@ using namespace std;
 class Solution{
 public:	
 	vector<int> kLargest(int arr[], int n, int k) {
-	    
 	    priority_queue<int> pq;
-	    for(int i=0;i<n;i++)
-	        pq.push(arr[i]);
-	        
-	    vector<int> ans;
-	    while(k--) {
-	        ans.push_back(pq.top());
+	    for(int i = 0; i < n; i++) {
+	        pq.push(-arr[i]);
+	        if(pq.size() > k) pq.pop();
+	    }
+	    vector<int> ans(k);
+	    for(int i = k - 1; i >= 0; i--) {
+	        ans[i] = -pq.top();
 	        pq.pop();
 	    }
 	    return ans;
-	    
 	}
 
 };
