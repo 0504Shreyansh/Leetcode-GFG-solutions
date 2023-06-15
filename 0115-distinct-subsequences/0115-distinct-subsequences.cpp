bca7bfbc1cab955c1d6f9ca8e49bf06e6e37b757
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-15 10:41:51 +0530

    Time: 31 ms (86.97%), Space: 6.4 MB (96.39%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    unsigned long long solve(int i, int j, string &s, string &t, vector<vector<unsigned long long>>& dp) {
-        if(j == 0) return 1;
-        if(i == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i - 1] == t[j - 1]) return dp[i][j] = (solve(i - 1, j - 1, s, t, dp) + solve(i - 1, j, s, t, dp));
-        return dp[i][j] = solve(i - 1, j, s, t, dp);
-    }
-
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
@@ -15,21 +7,15 @@ public:
         vector<unsigned long long> dp(m + 1, 0);
         
         // Base case
-        for(int j = 0; j <= m; j++)
-            prev[j] = 0;
-        prev[0] = 1;
+        // prev[0] = 1;
         
         for(int i = 1; i <= n; i++) {
             dp[0] = 1;
-            for(int j = 1; j <= m; j++) {
+            for(int j = m; j >= 1; j--) {
                 if(s[i - 1] == t[j - 1]) {
-                    dp[j] = prev[j - 1] + prev[j];
-                }
-                else {
-                    dp[j] = prev[j];
+                    dp[j] += dp[j - 1];
                 }
             }
-            prev = dp;
         }
         return dp[m];
     }
