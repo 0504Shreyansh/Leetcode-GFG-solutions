849f7dd88dcc7b61a6c945035c2e42ae733df0ac
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-15 10:58:48 +0530

    Time: 606 ms (62.63%), Space: 116.6 MB (77.78%) - LeetHub

--- 1001-grid-illumination/1001-grid-illumination.cpp
+++ 1001-grid-illumination/1001-grid-illumination.cpp
@@ -5,15 +5,16 @@ public:
         unordered_map<int,int> col;
         unordered_map<int,int> plusD;
         unordered_map<int,int> negD;
-        map<pair<int,int>,int> lamp;
+        set<pair<int,int>> lampPos;
         
         for(auto &it : lamps) {
-            if(lamp[{it[0], it[1]}]) continue;
+            // consider multiple lamps as one 
+            if(lampPos.count({it[0], it[1]})) continue;
             row[it[0]]++;
             col[it[1]]++;
             plusD[it[0] + it[1]]++;
             negD[it[1] - it[0]]++;
-            lamp[{it[0], it[1]}]++;
+            lampPos.insert({it[0], it[1]});
         }
         
         vector<int> ans;
@@ -30,12 +31,12 @@ public:
                 for(int j = -1; j <= 1; j++) {
                     int R = r + i;
                     int C = c + j;
-                    if(lamp[{R, C}]) {
-                        if(--lamp[{R, C}] == 0) lamp.erase({R, C});
+                    if(lampPos.count({R, C})) {
+                        lampPos.erase({R, C});
                         if(--row[R] == 0) row.erase(R);
                         if(--col[C] == 0) col.erase(C);
-                        if(--plusD[R+C] == 0) plusD.erase(R+C);
-                        if(--negD[C-R] == 0) negD.erase(C-R);
+                        if(--plusD[R + C] == 0) plusD.erase(R + C);
+                        if(--negD[C - R] == 0) negD.erase(C - R);
                     }
                 }
             }
