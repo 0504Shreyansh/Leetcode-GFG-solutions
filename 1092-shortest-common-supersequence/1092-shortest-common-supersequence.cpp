95c0154fce9c53727687fde40c0cdc79a5e8dcff
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-17 20:51:57 +0530

    Time: 411 ms (5.06%), Space: 299.7 MB (5.00%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -3,39 +3,22 @@ public:
     string findLCS(string &str1, string &str2) {
         int n = str1.size();
         int m = str2.size();
-        vector<vector<int>> dp(n + 1, vector<int> (m + 1));
+        vector<vector<string>> dp(n + 1, vector<string> (m + 1));
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
-                if(str1[i - 1] == str2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];  
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+                dp[i][j] = (str1[i - 1] == str2[j - 1]) 
+                    ? (dp[i - 1][j - 1] + str1[i - 1]) : 
+                         (dp[i - 1][j].size() > dp[i][j - 1].size()) ? dp[i - 1][j] : dp[i][j - 1]; 
             }
         }        
-        string lcs;
-        int i = n, j = m;
-        while(i > 0 && j > 0) {
-            if(str1[i - 1] == str2[j - 1]) {
-                lcs += str1[i - 1];
-                i--, j--;
-            }
-            else if(dp[i - 1][j] == dp[i][j]) {
-                i--;
-            }
-            else {
-                j--;
-            }
-        }
-        reverse(begin(lcs),end(lcs));
-        return lcs;
+        return dp[n][m];
     }
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
+        int n = str1.size(), m = str2.size();
         string lcs = findLCS(str1, str2);
         int i = 0, j = 0, k = 0;
         string superString;
+        // take lcs and other non repeating characters
         while(i < n && j < m && k < lcs.size()) {
             while(str1[i] != lcs[k]) 
                 superString += str1[i++];
