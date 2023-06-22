5cd9d91d6bc9f8faedf2fa3a2f2539b2cca9d227
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-22 22:47:07 +0530

    Time: 50 ms (15.87%), Space: 28.6 MB (28.82%) - LeetHub

--- 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
+++ 0449-serialize-and-deserialize-bst/0449-serialize-and-deserialize-bst.cpp
@@ -12,69 +12,23 @@ public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-        if(root == NULL) {
-            return "#";
-        }
-        string ans = to_string(root->val) + ',';
-        queue<TreeNode*> q({root});
-        while(q.size()) {
-            int size = q.size();
-            while(size--) {
-                TreeNode* curr = q.front();
-                q.pop();
-                if(!curr) continue;
-                if(curr->left) {
-                    q.push(curr->left);
-                    ans += to_string(curr->left->val) + ',';
-                } else {
-                    q.push(NULL);
-                    ans += "#,";
-                }
-                if(curr->right) {
-                    q.push(curr->right);
-                    ans += to_string(curr->right->val) + ',';
-                } else {
-                    q.push(NULL);
-                    ans += "#,";
-                }
-            }
-        }
-        ans.pop_back();
-        return ans;
+        return (root == NULL) ? "#" :
+            to_string(root->val) + "#" + serialize(root->left) + serialize(root->right);
     }
 
     // Decodes your encoded data to tree.
-    TreeNode* deserialize(string data) {
-        // cout << data << endl;
-        if(data[0] == '#') {
-            return NULL;
-        }
-        vector<string> nodes;
-        stringstream ss(data);
-        string word;
-        while(std::getline(ss, word, ','))
-            nodes.push_back(word);
-        TreeNode *root = new TreeNode(stoi(nodes[0]));
-        int i = 1;
-        queue<TreeNode*> q({root});
-        while(q.size()) {
-            int size = q.size();
-            while(size--) {
-                TreeNode *curr = q.front();
-                q.pop();
-                if(nodes[i] != "#") {
-                    curr -> left = new TreeNode(stoi(nodes[i]));
-                    q.push(curr->left);
-                }
-                i++;
-                if(nodes[i] != "#") {
-                    curr -> right = new TreeNode(stoi(nodes[i]));
-                    q.push(curr->right);
-                } 
-                i++;
-            }
-        }
+    TreeNode* createTree(stringstream& ss) {
+        string s;
+        getline(ss, s, '#');
+        if(s.size() == 0) return NULL;
+        TreeNode* root = new TreeNode(stoi(s));
+        root->left = createTree(ss);
+        root->right = createTree(ss);
         return root;
+    }
+    TreeNode* deserialize(string data) {
+        stringstream ss(data);
+        return createTree(ss);
     }
 };
 
