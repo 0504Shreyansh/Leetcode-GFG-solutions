23c661c6bf8f7cac4a1954960ce2e1bd80b5d5f1
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-24 11:13:07 +0530

    Added solution - LeetHub

--- Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
+++ Prefix match with other strings - GFG/prefix-match-with-other-strings.cpp
@@ -7,78 +7,19 @@ using namespace std;
 // } Driver Code Ends
 //User function Template for C++
 
-struct TrieNode {
-    TrieNode *childNode[26];
-    int cntPrefix = 0;
-    int cntEnd = 0;
-    bool containsKey(char ch) {
-        return childNode[ch - 'a'] != NULL;
-    }
-    void put(char ch, TrieNode *root) {
-        childNode[ch - 'a'] = root;
-    }
-    TrieNode* get(char ch) {
-        return childNode[ch - 'a'];
-    }
-    void increasePrefix() {
-        cntPrefix++;
-    }
-    void decreasePrefix() {
-        cntPrefix--;
-    }
-    void increaseEnd() {
-        cntEnd++;
-    }
-    void decreaseEnd() {
-        cntEnd--;
-    }
-    int getPrefix() {
-        return cntPrefix;
-    }
-    int getEnd() {
-        return cntEnd;
-    }
-};
-
-class Trie {
-    private:
-    TrieNode* root;
-    public:
-    Trie() {
-        root = new TrieNode();
-    }
-    // Inserts a key
-    void insert_key(string word) {
-        TrieNode *node = root;
-        for(int i = 0; i < word.size(); i++) {
-            if(!node->containsKey(word[i])) {
-                node->put(word[i], new TrieNode());
-            }
-            node = node->get(word[i]);
-            node -> increasePrefix();
-        }
-        node->increaseEnd();
-    }
-    int countStartWith(string word) {
-        TrieNode *node = root;
-        for(int i = 0; i < word.size(); i++) {
-            if(!node->containsKey(word[i])) {
-                return 0;
-            }
-            node = node->get(word[i]);
-        }
-        return node->getPrefix();
-    }
-};
-
-
 class Solution{   
 public:
-    int klengthpref(string arr[], int n, int k, string ss){    
-        Trie *root = new Trie();
-        for(int i = 0; i < n; i++)
-            root -> insert_key(arr[i]);
-        return root -> countStartWith(ss.substr(0, k));
+    int klengthpref(string arr[], int n, int k, string str){    
+        if(k > str.size()) return 0;
+        str = str.substr(0, k);
+        int res = 0;
+        for(int i = 0; i < n; i++) {
+            if(arr[i].substr(0, k) == str) {
+                // cout << arr[i] << ' ' << str << endl;
+                res++;
+            } 
+        }
+        return res;
     }
 };
 
