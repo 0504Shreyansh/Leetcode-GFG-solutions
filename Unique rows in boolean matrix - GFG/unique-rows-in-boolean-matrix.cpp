4ce13f37f1e17a6c6779eb5203fe34955d9871cc
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-25 08:41:35 +0530

    Added solution - LeetHub

--- Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
+++ Unique rows in boolean matrix - GFG/unique-rows-in-boolean-matrix.cpp
@@ -5,6 +5,86 @@ using namespace std;
 
 vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col);
 
+
+// } Driver Code Ends
+
+struct TrieNode {
+    TrieNode* childNode[2];
+    bool end = false;
+    bool containsKey(int a) {
+        return childNode[a] != NULL;
+    }
+    void put(int a, TrieNode* curr) {
+        childNode[a] = curr;
+    }
+    TrieNode *get(int a) {
+        return childNode[a];
+    }
+    void setEnd() {
+        end = true;
+    }
+    void unsetEnd() {
+        end = false;
+    }
+    bool getEnd() {
+        return end;
+    }
+};
+
+class Trie {
+    private:
+    TrieNode* root;
+    public:
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(int *row, int col) {
+        TrieNode *node = root;
+        for(int i = 0; i < col; i++) {
+            if(!node->containsKey(row[i])) {
+                node->put(row[i], new TrieNode());
+            }
+            node = node->get(row[i]);
+        }
+        node->setEnd();
+    }
+    bool search(int *row, int col) {
+        TrieNode *node = root;
+        for(int i = 0; i < col; i++) {    
+            node = node->get(row[i]);
+        }
+        bool ans = node->getEnd();
+        node->unsetEnd();
+        return ans;
+    }
+};
+
+class Solution
+{
+    public:
+    // #define MAX 1000
+    vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
+    {
+        Trie *root = new Trie();
+        for(int i = 0; i < row; i++) {
+            root->insert(M[i], col);
+        }
+
+        vector<vector<int>> ans;
+        for(int i = 0; i < row; i++) {
+            vector<int> tmp;
+            if(root->search(M[i], col)) {
+                for(int j = 0; j < col; j++)
+                    tmp.push_back(M[i][j]);
+                ans.push_back(tmp);
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
 int main()
 {
     int T;
@@ -17,7 +97,8 @@ int main()
     	for(int i=0;i<n;i++)
     	for(int j=0;j<m;j++)
     	cin>>a[i][j];
-    	vector<vector<int>> vec = uniqueRow(a,n,m);
+    	Solution ob;
+    	vector<vector<int>> vec = ob.uniqueRow(a,n,m);
     	for(int i = 0;i<vec.size();i++){
     	    for(int x : vec[i]){
     	        cout<<x<<" ";
@@ -29,64 +110,3 @@ int main()
 }
 
 // } Driver Code Ends
-
-struct TrieNode {
-    TrieNode *childNode[2];
-    bool isEnd = false;
-    bool containsKey(int a) {
-        return childNode[a] != NULL;
-    }
-    void put(int a, TrieNode* root) {
-        childNode[a] = root;
-    }
-    TrieNode* get(int a) {
-        return childNode[a]; 
-    }
-    bool getEnd() {
-        return isEnd;
-    }
-    void setEnd() {
-        isEnd = true;
-    }
-};
-
-class Trie {
-    public:
-    TrieNode *root;
-    Trie() {
-        root = new TrieNode();
-    }
-    void insert(int row[], bool &toPush, int n) {
-        TrieNode* node = root;
-        for(int i = 0; i < n; i++) {
-            if(!node->containsKey(row[i])) {
-                node -> put(row[i], new TrieNode());
-            }
-            node = node -> get(row[i]);
-        }
-        if(node -> getEnd()) {
-            return ;
-        }
-        node -> setEnd();
-        toPush = true;
-    }
-};
-
-
-/*You are required to complete this function*/
-vector<vector<int>> uniqueRow(int M[MAX][MAX],int row,int col)
-{
-    vector<vector<int>> ans;
-    Trie *root = new Trie();
-    for(int i = 0; i < row; i++) {
-        bool toPush = false;
-        root -> insert(M[i], toPush, col);
-        if(toPush) {
-            vector<int> tmp;
-            for(int j = 0; j < col; j++)
-                tmp.push_back(M[i][j]);
-            ans.push_back(tmp);
-        }
-    }
-    return ans;
-}
