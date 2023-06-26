9e280cd0a34175f39aa2de61a72407a6dcc15de9
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-26 08:17:17 +0530

    Time: 268 ms (30.69%), Space: 75.6 MB (44.55%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int n = costs.size();
+        int low = candidates - 1;
+        int high = max(low + 1, n - candidates);
+        priority_queue<pair<int,int>> pq;
+        for(int i = 0; i <= low; i++) {
+            pq.push({-costs[i], -i});
+        }   
+        for(int i = n - 1; i >= high; i--) {
+            pq.push({-costs[i], -i});
+        }
+        long long total_cost = 0;
+        while(pq.size() && k--) {
+            auto curr = pq.top();
+            pq.pop();
+            int curr_cost = -curr.first;
+            int index = -curr.second;
+            total_cost += curr_cost;
+            if(index >= high) {
+                high--;
+                if(low < high) pq.push({-costs[high], -high});
+            } else {
+                low++;
+                if(low < high) pq.push({-costs[low], -low});
+            }
+        }
+        return total_cost;
+    }
+};
