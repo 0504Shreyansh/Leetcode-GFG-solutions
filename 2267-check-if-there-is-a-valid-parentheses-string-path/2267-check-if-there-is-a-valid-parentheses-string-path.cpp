eb2e54f48994f84efe78f388fa1faaf940aef8cb
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-26 08:56:27 +0530

    Time: 642 ms (26.80%), Space: 223.1 MB (25.26%) - LeetHub

--- 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
+++ 2267-check-if-there-is-a-valid-parentheses-string-path/2267-check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     int solve(int i, int j, int ob_cb, vector<vector<char>>& grid, vector<vector<vector<int>>>& dp) {
-        int n = grid.size(), m = grid[i].size();
-        ob_cb += (grid[i][j] == '(') ? 1 : -1;
-        if(ob_cb < 0) return 0;
-        if(i == n - 1 && j == m - 1) {
-            return (ob_cb == 0);
+        if(i >= grid.size() || j >= grid[0].size()) {
+            return false;
+        }
+        if(grid[i][j] == ')' && ob_cb == 0) {
+            return false;
         }
         if(dp[i][j][ob_cb] != -1) {
             return dp[i][j][ob_cb];
         }
-        int paths = 0;
-        if(i + 1 < n && solve(i + 1, j, ob_cb, grid, dp)) return dp[i][j][ob_cb] = true;
-        if(j + 1 < m && solve(i, j + 1, ob_cb, grid, dp)) return dp[i][j][ob_cb] = true;
-        return dp[i][j][ob_cb] = false;
+        if(i == grid.size() - 1 && j == grid[0].size() - 1) {
+            return ob_cb + ((grid[i][j] == '(') ? 1 : -1) == 0;
+        }
+        return dp[i][j][ob_cb] = solve(i + 1, j, ob_cb + ((grid[i][j] == '(') ? 1 : -1), grid, dp) |
+                                solve(i, j + 1, ob_cb + ((grid[i][j] == '(') ? 1 : -1), grid, dp);
     }
-
     bool hasValidPath(vector<vector<char>>& grid) {
-        if(grid[0][0] == ')') return false;
         int n = grid.size(), m = grid[0].size();
-        if((m+n-1)&1) return false;   // not possible
-        vector<vector<vector<int>>> dp(n, 
-            vector<vector<int>> (m, vector<int> (n+m+1, -1)));
+        vector<vector<vector<int>>> dp(n + 1, 
+            vector<vector<int>> (m + 1, vector<int> (n + m + 1, -1)));
         return solve(0, 0, 0, grid, dp);
     }
 };
