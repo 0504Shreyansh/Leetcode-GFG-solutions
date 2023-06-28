afed3667adc71e3187df722ab7622009548cef98
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-28 22:35:50 +0530

    Time: 140 ms (20.00%), Space: 31.3 MB (21.39%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.cpp
@@ -11,20 +11,14 @@ private:
     }
 
     int solve(int i, string &s, int k, vector<vector<int>>& dp) {
-        if(i >= s.size()) {
-            return 1e5;
-        }
-        if(k == 1) {
-            return changes(s.substr(i, s.size() - i));
-        }
+        if(i >= s.size()) return 1e5;
+        if(k == 1) return changes(s.substr(i, s.size() - i));
         if(dp[i][k] != -1) return dp[i][k];
         string curr;
         int res = INT_MAX;
         for(int j = i; j < s.size(); ++j) {
             curr += s[j];
-            int cur = changes(curr) + solve(j + 1, s, k - 1, dp);
-            // cout << curr << ' ' << cur << endl; 
-            res = min(res, cur);
+            res = min(res, changes(curr) + solve(j + 1, s, k - 1, dp));
         }
         return dp[i][k] = res;
     } 
