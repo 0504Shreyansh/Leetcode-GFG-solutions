a8769afbc2fa32f9d3a16c79ae98b5774c868aaf
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-06-29 20:28:15 +0530

    Time: 9 ms (41.78%), Space: 9.6 MB (5.60%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int solve(int i, int j, bool turn, vector<int>& A) {
+    int solve(int i, int j, bool turn, vector<int>& A, vector<vector<vector<int>>>& dp) {
         if(i > j) return 0;
-        if(turn) return max(A[i] + solve(i + 1, j, !turn, A), A[j] + solve(i, j - 1, !turn, A));
-        return min(solve(i + 1, j, !turn, A), solve(i, j - 1, !turn, A));
+        if(dp[i][j][turn] != -1) return dp[i][j][turn];
+        if(turn) return dp[i][j][turn] = max(A[i] + solve(i + 1, j, !turn, A, dp), A[j] + solve(i, j - 1, !turn, A, dp));
+        return dp[i][j][turn] = min(solve(i + 1, j, !turn, A, dp), solve(i, j - 1, !turn, A, dp));
     }
     bool PredictTheWinner(vector<int>& A) {
         int total_sum = 0;
         for(auto &it : A) {
             total_sum += it;
         }
-        int curr_sum = solve(0, A.size() - 1, true, A);
+        vector<vector<vector<int>>> dp(21, 
+            vector<vector<int>> (21, vector<int> (2, -1)));
+        int curr_sum = solve(0, A.size() - 1, true, A, dp);
         return total_sum - curr_sum <= curr_sum;
     }
 };
