8e4a2ec3a01b165d177e6f03ce1f5e2381eff5c9
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-01 10:59:43 +0530

    Time: 27 ms (71.43%), Space: 9.4 MB (80.36%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    int solve(int i, int mask, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp) {
+    int solve(int i, int mask, vector<int>& nums1, vector<int>& nums2, vector<int>& dp) {
         int n = nums1.size();
         if(i >= n) {
             return 0;
         }
-        if(dp[i][mask] != -1) {
-            return dp[i][mask];
+        if(dp[mask] != -1) {
+            return dp[mask];
         }
         int ans = INT_MAX;
         for(int j = 0; j < n; ++j) {
@@ -15,10 +15,10 @@ public:
                 ans = min(ans, curr);
             }
         }
-        return dp[i][mask] = ans;
+        return dp[mask] = ans;
     }
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> dp(nums1.size(), vector<int> (1 << nums1.size(), -1));
+        vector<int> dp(1 << nums1.size(), -1);
         return solve(0, 0, nums1, nums2, dp);
     }
 };
