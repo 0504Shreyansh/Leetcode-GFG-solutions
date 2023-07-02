2ab5b5e1b0ac34b0db9223a9bf911df0fe1fe214
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-02 11:22:43 +0530

    Time: 37 ms (90.96%), Space: 23.9 MB (53.19%) - LeetHub

--- 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
+++ 2172-maximum-and-sum-of-array/2172-maximum-and-sum-of-array.cpp
@@ -1,5 +1,5 @@
 class Solution {
-public:
+private:
     int solve(int i, int mask1, int mask2, int numSlots, vector<int>& nums, vector<vector<int>>& dp) {
         if(i >= nums.size()) {
             return 0;
@@ -10,21 +10,20 @@ public:
         int ans = INT_MIN;
         for(int m = 0; m < numSlots; m++) {
             if((mask1 & (1 << m)) == 0) {
-                int curr = (nums[i] & (m + 1)) + solve(i + 1, mask1 | (1 << m), mask2, numSlots, nums, dp);
+                int curr = (nums[i] & (m + 1)) + 
+                    solve(i + 1, mask1 | (1 << m), mask2, numSlots, nums, dp);
                 ans = max(ans, curr);
             } else if((mask2 & (1 << m)) == 0) {
-                int curr = (nums[i] & (m + 1)) + solve(i + 1, mask1, mask2 | (1 << m), numSlots, nums, dp);
+                int curr = (nums[i] & (m + 1)) + 
+                    solve(i + 1, mask1, mask2 | (1 << m), numSlots, nums, dp);
                 ans = max(ans, curr);
             }
         }
         return dp[mask1][mask2] = ans;
     }
 
-    // Leetcode --> Maximum AND Sum of an Array
+public:
     int maximumANDSum(vector<int>& nums, int numSlots) {
-        // vector<vector<vector<int>>> dp(nums.size(), 
-        //     vector<vector<int>> (1 << numSlots, 
-        //         vector<int> (1 << numSlots, -1)));
         vector<vector<int>> dp(1 << numSlots, vector<int> (1 << numSlots, -1));
         return solve(0, 0, 0, numSlots, nums, dp);
     }
