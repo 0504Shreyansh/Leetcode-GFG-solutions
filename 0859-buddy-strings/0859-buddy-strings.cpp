6390e77919b9c42d969431911b749aad5002b3c9
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-03 09:49:52 +0530

    Time: 32 ms (5.45%), Space: 11.7 MB (5.02%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.cpp
+++ 0859-buddy-strings/0859-buddy-strings.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool buddyStrings(string s, string goal) {
+        // edge cases
         if(s.size() != goal.size() || (s.size() == 1 && goal.size() == 1)) {
             return false;
         }
@@ -10,7 +11,9 @@ public:
             if(++mp[s[i]] == 0) mp.erase(s[i]);
             if(--mp[goal[i]] == 0) mp.erase(goal[i]);
         }
+        // some different chars
         if(mp.size() != 0) return false;
+        
         int idx1 = -1, idx2 = -1;
         char c1, c2;
         for(int i = 0; i < n; i++) {
@@ -29,17 +32,19 @@ public:
         }
         set<char> s1(begin(s),end(s));
         set<char> s2(begin(goal),end(goal));
+        
+        // same set of chars
         if(s1.size() == s2.size()) {
-            if(s1.size() == 1) {
+            if(s1.size() == 1) {   // all chars are same
                 if(*s1.begin() == *s2.begin()) return true;
                 return false;  
-            } else if(n != s1.size()) {
+            } else if(n != s1.size()) {   
                 return true;
             }
         } 
         if(s == goal) return false;
-        if(idx1 == -1 && idx2 == -1) return false;
-        if(idx2 == -1) return false;
+        // if(idx1 == -1 && idx2 == -1) return false;
+        // if(idx2 == -1) return false;
         return true;
     }
 };
