41462a36304585aab0e0e8e9e87b6cb89c54cdc3
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-05 08:12:43 +0530

    Time: 36 ms (93.29%), Space: 36.5 MB (65.98%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
+        /*1 1 1 1 --> 4 1 1 4*/
+        /*4 1 1 4 0 3 1 3 --> 1 + 3 + 4*/
         int n = nums.size();
         int res = 0;
         for(int i = 0; i < n; i++) {
+            // mark the starting and ending index to the 
+            // size of corresponding size of chunk of 1's
             if(nums[i] == 1) {
                 int index = i;
                 while(index < n && nums[index] == 1) {
@@ -16,6 +20,7 @@ public:
                 if(i < index) i = index;
             }
         }
+        // look for (non-zero 0 non-zero) for maximum size
         for(int i = 1; i < n - 1; i++) {
             if(nums[i] == 0) {
                 res = max(res, nums[i - 1] + nums[i + 1]);
