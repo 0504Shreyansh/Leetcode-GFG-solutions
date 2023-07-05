4743193579c030ae6812bb8c316b8e88f286abb3
0504Shreyansh <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-05 18:55:16 +0530

    Time: 11 ms (19.88%), Space: 15.3 MB (6.42%) - LeetHub

--- 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.cpp
+++ 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.cpp
@@ -1,35 +1,37 @@
 class Solution {
-public:
+private:
     bool isPerfectSquare(int a, int b) {
         int sqroot = sqrt(a + b);
         return sqroot * sqroot == (a + b);
     }
 
     int solve(int prev, int mask, vector<int>& nums, vector<vector<int>>& dp) {
-        if(mask == (1 << (int)nums.size()) - 1) {
+        int n = nums.size();
+        if(mask == (1 << n) - 1) {
             return 1;
         }
         if(dp[prev][mask] != -1) {
             return dp[prev][mask];
         }
         int ans = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            if(!(mask & (1 << i)) && isPerfectSquare(nums[prev], nums[i])) {
-                ans += solve(i, mask | (1 << i), nums, dp);
-            }
+        for(int i = 0; i < n; i++) {
+            if(mask & (1 << i)) continue;
+            if(!isPerfectSquare(nums[prev], nums[i])) continue;
+            ans += solve(i, mask | (1 << i), nums, dp);
         }
         return dp[prev][mask] = ans;
     }
 
+public:
     int numSquarefulPerms(vector<int>& nums) {
-
-        unordered_map<int, int> freq;
-        for(auto &x : nums) freq[x]++;
+        int n = nums.size();
         int mask = 0;
         int ans = 0;
-        vector<vector<int>> dp(nums.size() + 1, vector<int> ((1 << nums.size() + 1), -1));
-        for(int i = 0; i < nums.size(); i++) {
-            if(!(mask & (1 << i))) {
+        vector<vector<int>> dp(n + 1, vector<int> ((1 << n + 1), -1));
+        unordered_map<int, int> freq;
+        for(int i = 0; i < n; i++) {
+            freq[nums[i]]++;
+            if(!(mask & (1 << i))) {  // starting point
                 ans += solve(i, mask | (1 << i), nums, dp);
             }
         }
@@ -37,9 +39,8 @@ public:
         for(int i = 1; i <= 12; ++i) {
             fact[i] = i * fact[i - 1];
         }
-        for(auto &it : freq) {
+        for(auto &it : freq) {  // decrease the repeated count
             ans /= fact[it.second];
-            // cout << fact[freq[nums[i]]] << endl;
         }
         return ans;
     }
