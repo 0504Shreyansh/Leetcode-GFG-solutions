729b0c35e0b622907045c660d5f90df58f0736db
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-07 21:06:33 +0530

    Time: 280 ms (15.97%), Space: 26.6 MB (7.64%) - LeetHub

--- 1595-minimum-cost-to-connect-two-groups-of-points/1595-minimum-cost-to-connect-two-groups-of-points.cpp
+++ 1595-minimum-cost-to-connect-two-groups-of-points/1595-minimum-cost-to-connect-two-groups-of-points.cpp
@@ -1,6 +1,7 @@
 class Solution {
-public:
-    int solve(int i, int j, int mask, vector<vector<int>>& cost, vector<vector<vector<int>>>& dp) {
+private:
+    int solve(int i, int j, int mask, vector<vector<int>>& cost,
+              vector<vector<vector<int>>>& dp) {
         if(i >= cost.size()) {
             if(mask == (1 << cost[0].size()) - 1) {
                 return 0;
@@ -10,13 +11,19 @@ public:
         if(dp[i][j][mask] != -1) {
             return dp[i][j][mask];
         }
-        int takeInRow = 1e5, takeInNext = 1e5, notTake = 1e5;
-        if(j + 1 != cost[i].size()) notTake = solve(i, j + 1, mask, cost, dp);
-        if(j + 1 != cost[i].size()) takeInRow = cost[i][j] + solve(i, j + 1, mask | (1 << j), cost, dp);
-        takeInNext = cost[i][j] + solve(i + 1, 0, mask | (1 << j), cost, dp);
-        return dp[i][j][mask] = min({notTake, takeInNext, takeInRow});
+        int takeAndStayInRow = 1e5, skipValueAndStayInRow = 1e5;
+        if(j + 1 != cost[i].size()) 
+            skipValueAndStayInRow = solve(i, j + 1, mask, cost, dp);
+
+        if(j + 1 != cost[i].size()) 
+            takeAndStayInRow = cost[i][j] + solve(i, j + 1, mask | (1 << j), cost, dp);
+
+        int takeAndSkipRow = cost[i][j] + solve(i + 1, 0, mask | (1 << j), cost, dp);
+        return dp[i][j][mask] = min({skipValueAndStayInRow, 
+                                     takeAndSkipRow, takeAndStayInRow});
     }
 
+public:
     int connectTwoGroups(vector<vector<int>>& cost) {
         vector<vector<vector<int>>> dp(cost.size(), 
             vector<vector<int>> (cost[0].size(), 
