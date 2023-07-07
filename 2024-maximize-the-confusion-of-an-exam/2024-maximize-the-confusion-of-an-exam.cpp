55a823f393cc15ebf7f6c1367941757b2d6996d4
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-07 10:34:15 +0530

    Time: 63 ms (27.25%), Space: 14.7 MB (7.97%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -3,53 +3,38 @@ public:
     int maxConsecutiveAnswers(string answerKey, int k) {
 
         // T --> F
-        int i = 0, j = 0;
-        int curr1 = 0;
         int n = answerKey.size();
+        int i = 0;
+        int ans = 0;
         deque<int> dq;
         for(int j = 0; j < n; ++j) {
             if(answerKey[j] == 'F') {
-                if(dq.size() < k) {
-                    dq.push_back(j);
-                }
-                else {
-                    while(dq.size() == k && i <= j) {
-                        if(dq.front() == i) {
-                            dq.pop_front();
-                        }
-                        i++;
+                while(dq.size() == k && i <= j) {
+                    if(dq.front() == i) {
+                        dq.pop_front();
                     }
-                    dq.push_back(j);
+                    i++;
                 }
+                dq.push_back(j);    
             }
-            curr1 = max(curr1, j - i + 1);
+            ans = max(ans, j - i + 1);
         }
-
 
         // F --> T
-        int i2 = 0, j2 = 0;
-        deque<int> dq2;
-        int curr2 = 0;
-        for(int j2 = 0; j2 < n; ++j2) {
-            if(answerKey[j2] == 'T') {
-                if(dq2.size() < k) {
-                    dq2.push_back(j2);
-                }
-                else {
-                    while(dq2.size() == k && i2 <= j2) {
-                        if(dq2.front() == i2) {
-                            dq2.pop_front();
-                        }
-                        i2++;
+        i = 0;
+        dq.clear();
+        for(int j = 0; j < n; ++j) {
+            if(answerKey[j] == 'T') {
+                while(dq.size() == k && i <= j) {
+                    if(dq.front() == i) {
+                        dq.pop_front();
                     }
-                    dq2.push_back(j2);
+                    i++;
                 }
+                dq.push_back(j);
             }
-            curr2 = max(curr2, j2 - i2 + 1);
+            ans = max(ans, j - i + 1);
         }
-
-        // cout << curr1 << ' ' << curr2 << endl;
-        int ans = max(curr1, curr2);
         return ans;
     }
 };
