f909321cd45764ba18ff4c4231a96f19e2a2665f
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-07 21:15:09 +0530

    Time: 225 ms (18.75%), Space: 29.9 MB (6.94%) - LeetHub

--- 1595-minimum-cost-to-connect-two-groups-of-points/1595-minimum-cost-to-connect-two-groups-of-points.cpp
+++ 1595-minimum-cost-to-connect-two-groups-of-points/1595-minimum-cost-to-connect-two-groups-of-points.cpp
@@ -1,36 +1,11 @@
 class Solution {
-private:
-    int solve(int i, int j, int mask, vector<vector<int>>& cost,
-              vector<vector<vector<int>>>& dp) {
-        if(i >= cost.size()) {
-            if(mask == (1 << cost[0].size()) - 1) {
-                return 0;
-            }
-            return 1e5;
-        }
-        if(dp[i][j][mask] != -1) {
-            return dp[i][j][mask];
-        }
-        int takeAndStayInRow = 1e5, skipValueAndStayInRow = 1e5;
-        if(j + 1 != cost[i].size()) 
-            skipValueAndStayInRow = solve(i, j + 1, mask, cost, dp);
-
-        if(j + 1 != cost[i].size()) 
-            takeAndStayInRow = cost[i][j] + solve(i, j + 1, mask | (1 << j), cost, dp);
-
-        int takeAndSkipRow = cost[i][j] + solve(i + 1, 0, mask | (1 << j), cost, dp);
-        return dp[i][j][mask] = min({skipValueAndStayInRow, 
-                                     takeAndSkipRow, takeAndStayInRow});
-    }
-
 public:
     int connectTwoGroups(vector<vector<int>>& cost) {
         int n = cost.size();
         int m = cost[0].size();
         vector<vector<vector<int>>> dp(n + 1, 
             vector<vector<int>> (m + 1, 
-                vector<int> ((1 << m), 1e5)));
-        // return solve(0, 0, 0, cost, dp);
+                vector<int> ((1 << m), 1e5)));  // --> dp[0][0][0]
         
         for(int j = 0; j < m; j++) {
             dp[n][j][(1<<m)-1] = 0;
@@ -40,10 +15,10 @@ public:
             for(int j = m - 1; j >= 0; j--) {
                 for(int mm = 0; mm < (1<<m); mm++) {
                     int takeAndStayInRow = 1e5, skipValueAndStayInRow = 1e5;
-                    if(j + 1 != cost[i].size()) 
+                    if(j + 1 != m) 
                         skipValueAndStayInRow = dp[i][j + 1][mm];
 
-                    if(j + 1 != cost[i].size()) 
+                    if(j + 1 != m) 
                         takeAndStayInRow = cost[i][j] + dp[i][j + 1][mm | (1 << j)];
 
                     int takeAndSkipRow = cost[i][j] + dp[i + 1][0][mm | (1 << j)];
@@ -54,6 +29,5 @@ public:
         }
         
         return dp[0][0][0];
-        
     }
 };
