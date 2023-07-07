c28b8751d101875c071da431d7695c30e82fe3d2
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-07 10:44:42 +0530

    Time: 153 ms (6.81%), Space: 27.6 MB (5.14%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+        if(k == 0) {
+            for(int i = 0; i < n; i++) {
+                int j = i;
+                while(j + 1 < n && s[j + 1] == s[i]) j++;
+                ans = max(ans, j - i + 1);
+                i = j;
+            }
+            return ans;
+        }
+        // change to each possible chars
+        for(char ch = 'A'; ch <= 'Z'; ++ch) {
+            int i = 0;
+            deque<int> dq;
+            int curr = 0;
+            for(int j = 0; j < n; ++j) {
+                if(s[j] != ch) {
+                    while(!dq.empty() && dq.size() == k && i <= j) {
+                        if(dq.front() == i) {
+                            dq.pop_front();
+                        }
+                        i++;
+                    }
+                    dq.push_back(j);    
+                }
+                curr = max(curr, j - i + 1);
+            }
+            ans = max(ans, curr);
+            cout << curr << ' ';
+        }
+        return ans;
+    }
+};
