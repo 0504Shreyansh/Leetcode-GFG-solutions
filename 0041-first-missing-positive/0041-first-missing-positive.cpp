3286111b799fea8f07f7dd3556090ac7f4378e7d
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-12 16:29:51 +0530

    Time: 88 ms (35.15%), Space: 41.1 MB (91.77%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,20 +1,25 @@
-class Solution
-{
-    public:
-        int firstMissingPositive(vector<int> &nums)
-        {
-
-            map<int, int> mp;
-            for (auto i: nums)
-            {
-                if (i > 0) mp[i]++;
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int i = 0;
+        int n = nums.size();
+        while (i < n) {
+            if(nums[i] == INT_MIN) {
+                i++;
+                continue;
             }
-            int cnt = 1;
-            for (auto i: mp)
-            {
-                if (cnt != i.first) return cnt;
-                else cnt++;
+            int pos = nums[i] - 1;
+            if (pos >= 0 && pos < n && nums[i] != nums[pos]) {
+                swap(nums[i], nums[pos]);
+            } else {
+                i++;   
             }
-            return cnt;
         }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
 };
