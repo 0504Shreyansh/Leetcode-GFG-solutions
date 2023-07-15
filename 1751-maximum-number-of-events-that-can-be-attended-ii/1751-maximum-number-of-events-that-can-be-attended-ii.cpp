93c3b6db17f109abe8338aa1de256a8fe9c28814
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-15 12:37:24 +0530

    Time: 335 ms (66.35%), Space: 72.8 MB (71.72%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,13 +1,6 @@
 class Solution {
 private:
-    int f(int i, vector<vector<int>>& events, int k, vector<vector<int>>& dp) {
-        if(i >= events.size() || k == 0) {
-            return 0;
-        }
-        if(dp[i][k] != -1) {
-            return dp[i][k];
-        }
-        int notPick = f(i + 1, events, k, dp), pick = 0;
+    int findNextPossibleIndex(int i, vector<vector<int>>& events) {
         int low = i; 
         int high = events.size() - 1;
         while(low <= high) {
@@ -18,7 +11,18 @@ private:
                 low = mid + 1;
             }
         }
-        pick = events[i][2] + f(low, events, k - 1, dp);
+        return low;
+    }
+    int f(int i, vector<vector<int>>& events, int k, vector<vector<int>>& dp) {
+        if(i >= events.size() || k == 0) {
+            return 0;
+        }
+        if(dp[i][k] != -1) {
+            return dp[i][k];
+        }
+        int notPick = f(i + 1, events, k, dp), pick = 0;
+        int newIndex = findNextPossibleIndex(i, events);
+        pick = events[i][2] + f(newIndex, events, k - 1, dp);
         return dp[i][k] = max(pick, notPick);
     }
 
