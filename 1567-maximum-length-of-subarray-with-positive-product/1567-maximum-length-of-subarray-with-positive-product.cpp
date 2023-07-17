3cfecbda5c7c79eb5a536abbc234e12718f0a9ce
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-17 19:02:25 +0530

    Time: 118 ms (83.77%), Space: 57.7 MB (94.93%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -6,39 +6,26 @@ public:
         int negIndex = -1;
         int negCount = 0;
         int ans = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; ++i) {
             if(nums[i] == 0) {
                 zeroIndex = i;
                 negIndex = -1;
                 negCount = 0;
-            } else if (nums[i] < 0) {
+            } else if(nums[i] < 0) {
                 ++negCount;
                 if(negCount == 1) {
                     negIndex = i;
                 }
-                if(negCount % 2 == 0) {
-                    if(zeroIndex == -1) {
-                        ans = max(ans, i + 1);
-                    } else {
-                        ans = max(ans, i - zeroIndex);
-                    }
-                }
-            } else {    
-                if(negCount % 2 == 0) {
-                    if(zeroIndex == -1) {
-                        ans = max(ans, i + 1);
-                    } else {
-                        ans = max(ans, i - zeroIndex);
-                    }
-                } else {
-                    if(zeroIndex == -1) {
-                        ans = max(ans, i - negIndex);
-                    } else {
-                        ans = max(ans, i - max(zeroIndex, negIndex));
-                    }
-                }
             }
-            // cout << zeroIndex << ' ' << negIndex << ' ' << ans << endl;
+            if(negCount % 2 == 0) {
+                if(zeroIndex == -1) {
+                    ans = max(ans, i + 1);
+                } else {
+                    ans = max(ans, i - zeroIndex);
+                }
+            } else {   
+                ans = max(ans, i - negIndex);
+            }        
         }
         return ans;
     }
