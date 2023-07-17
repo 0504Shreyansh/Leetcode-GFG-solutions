eb92fb26283fe540583f69eda06d6f221cdeb3fe
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-17 19:04:48 +0530

    Time: 119 ms (82.15%), Space: 57.8 MB (94.93%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.cpp
@@ -7,23 +7,24 @@ public:
         int negCount = 0;
         int ans = 0;
         for (int i = 0; i < n; ++i) {
-            if(nums[i] == 0) {
+            if(nums[i] == 0) {  // restore the count and index of -ve number
                 zeroIndex = i;
                 negIndex = -1;
                 negCount = 0;
             } else if(nums[i] < 0) {
-                ++negCount;
+                ++negCount;  // first index of -ve number
                 if(negCount == 1) {
                     negIndex = i;
                 }
             }
+            // main logic 
             if(negCount % 2 == 0) {
-                if(zeroIndex == -1) {
+                if(zeroIndex == -1) {  // even -ve nums + no 0's
                     ans = max(ans, i + 1);
-                } else {
+                } else {    // even -ve nums + 0 found
                     ans = max(ans, i - zeroIndex);
                 }
-            } else {   
+            } else {     // odd -ve nums found --> zeroIndex < negIndex
                 ans = max(ans, i - negIndex);
             }        
         }
