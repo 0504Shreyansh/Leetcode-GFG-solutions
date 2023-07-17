7593a63d47e10ce30498adfbb75f71c3dcef29df
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-17 20:33:41 +0530

    Time: 98 ms (47.64%), Space: 18.8 MB (29.56%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -4,30 +4,26 @@ public:
         stack<char> st;
         int ans = 0;
         for (auto &it : directions) {
-            if(it == 'R') {
-                st.push(it);
-            } else if(it == 'L') {
-                if(st.size()) {
-                    if(st.top() == 'R') {
-                        ans += 2;
-                        st.pop();
-                        while(st.size() && st.top() == 'R') {
-                            ans++;  // ?
-                            st.pop();
-                        }
-                        st.push('S');
-                    
-                    } else if(st.top() == 'L') {
-                        st.push(it);
-                    } else {
-                        ans++;
-                        continue;
-                    }
-                } else {
+            if (it == 'L') {
+                if (st.empty()) {
                     st.push(it);
+                } else if (st.top() == 'S') {
+                    ans++;  // remove (S + L)'s collisions
+                } else if (st.top() == 'L') {
+                    st.push(it);
+                } else {   // (R + L) --> S
+                    ans += 2; 
+                    st.pop();  // further (R + S)'s collisions
+                    while (st.size() && st.top() == 'R') {
+                        ans++;
+                        st.pop();
+                    }
+                    st.push('S');
                 }
-            } else {
-                while(st.size() && st.top() == 'R') {
+            } else if (it == 'R') { // no collisions at the moment
+                st.push(it);
+            } else {  // remove (R + S)'s collisions
+                while (st.size() && st.top() == 'R') {
                     ans++;
                     st.pop();
                 }
