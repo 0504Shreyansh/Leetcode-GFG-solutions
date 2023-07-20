475342f75f98614fd2050ff238526b3f499cef55
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-20 21:17:14 +0530

    Time: 15 ms (71.71%), Space: 17.8 MB (55.66%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,34 +2,30 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         stack<int> st;
-        for(auto &it : asteroids) {
-            if(it > 0 || st.empty()) {
+        for (auto &it : asteroids) {
+            if (st.empty() || it > 0) {
+                st.push(it);
+            } else if (st.top() < 0 && it < 0) {
                 st.push(it);
             } else {
-                if(st.top() < 0) {
-                    st.push(it);
-                } else if(st.top() > abs(it)) {
-                    continue;
-                } else if(st.top() == abs(it)) {
+                while (st.size() && st.top() > 0 && it < 0 && st.top() < abs(it)) {
                     st.pop();
-                } else {
-                    bool f = 0;
-                    while(st.size() && st.top() > 0 && st.top() <= abs(it)) {
-                        if(st.top() == abs(it)) {
-                            st.pop(); f = 1;
-                            break;
-                        } st.pop();
-                    }
-                    if(st.size() && st.top() >= abs(it)) continue;
-                    if(!f) st.push(it);
+                }
+                if (st.empty()) {
+                    st.push(it);
+                } else if (st.top() < 0) {
+                    st.push(it);
+                } else if (st.top() == abs(it) && it < 0) {
+                    st.pop();
                 }
             }
-        }
-        vector<int> res(st.size());
-        while(st.size()) {
-            res[st.size() - 1] = st.top();
+        }   
+        vector<int> res;
+        while (st.size()) {
+            res.push_back(st.top());
             st.pop();
         }
+        reverse(begin(res),end(res));
         return res;
     }
 };
