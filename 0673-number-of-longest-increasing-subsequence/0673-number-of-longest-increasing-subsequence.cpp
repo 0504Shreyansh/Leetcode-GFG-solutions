223d22e74f9b6d37981bf8887dba92487edb0554
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-21 13:49:33 +0530

    Time: 157 ms (85.06%), Space: 13.2 MB (25.03%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -4,25 +4,27 @@ public:
         int N = nums.size();
         vector<int> dp(N, 1);
         vector<int> count(N, 1);
-        int maxLength = 1;
-        for(int i = 1; i < N; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[i] > nums[j]) {
-                    if(dp[i] < 1 + dp[j]) {
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    if (dp[i] < 1 + dp[j]) {
                         dp[i] = 1 + dp[j];
                         count[i] = count[j];
-                    }
-                    else if(dp[i] == 1 + dp[j]) {
+                    } else if (dp[i] == 1 + dp[j]) {
                         count[i] += count[j];
                     }
-                    maxLength = max(maxLength, dp[i]);
                 }
             }
         }
+        int maxLength = 1;
+        for (auto &len : dp) {
+            maxLength = max(maxLength, len);
+        }
         int ans = 0;
-        for(int i = 0; i < N; i++) {
-            if(dp[i] == maxLength)
+        for (int i = 0; i < N; i++) {
+            if (dp[i] == maxLength) {
                 ans += count[i];
+            }
         }
         return ans;
     }
