be8fa5e98f7d0cd15d62c143ffb63be22c874292
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-24 19:20:42 +0530

    Time: 71 ms (44.68%), Space: 78.1 MB (13.95%) - LeetHub

--- 2719-count-of-integers/2719-count-of-integers.cpp
+++ 2719-count-of-integers/2719-count-of-integers.cpp
@@ -1,60 +1,38 @@
 class Solution {
-public:
+private:
     const int mod = 1e9 + 7;
-    int build(string &s, int curIndex, int n, int sum, bool tight, vector<vector<vector<int>>> &dp) {
-        // Base case
-        if (n == 0) {
-            return (sum == 0) ? 1 : 0;
+    int solve(int i, string &s, int sum, int min_sum, int max_sum, bool tight, vector<vector<vector<int>>>& dp) {
+        if (i >= s.size()) {
+            return (min_sum <= sum && sum <= max_sum);
+        } 
+        if (dp[i][tight][sum] != -1) {
+            return dp[i][tight][sum];
         }
-
-        if (dp[n][sum][tight] != -1) {
-            return dp[n][sum][tight] % mod;
+        char upperLimit = (tight) ? s[i] : '9';
+        int ans = 0;
+        for (char c = '0'; c <= upperLimit; c++) {
+            ans += solve(i + 1, s, sum + (c - '0'), min_sum, max_sum, tight && (c == upperLimit), dp);
+            ans %= mod;
         }
-        
-        int answer = 0;
-        
-        char upperLimit = (tight) ? s[curIndex] : '9';
-
-        for (char ch = '0'; ch <= upperLimit; ch++) {
-            if (ch == s[curIndex] && tight) {
-                if (sum >= (ch - '0')) { // more tight conditions
-                    answer = (answer + build(s, curIndex + 1, n - 1, sum - (ch - '0'), tight, dp)) % mod;
-                }
-            }
-            else { // remove the tight from here
-                if (sum >= (ch - '0')) {
-                    answer = (answer + build(s, curIndex + 1, n - 1, sum - (ch - '0'), false, dp)) % mod;
-                }
-            }
-        }
-        return dp[n][sum][tight] = answer % mod;
+        return dp[i][tight][sum] = ans;
     }
-
-    void decreaseBy1(string &str) {
-        int n = str.size();
-        int i = n - 1;
-        while (i >= 0 && str[i] == '0') 
-            str[i--] = '9';
-        str[i] = str[i] - 1;
+    void decreaseBy1(string &s) {
+        int i = s.size() - 1;
+        while (s[i] == '0') {
+            s[i] = '9';
+            i--;
+        }
+        s[i] --;
     }
-
-    int count(string num1, string num2, int sum1, int sum2) {
-        
-        int n = num1.size();
-        int m = num2.size();
+public:
+    int count(string num1, string num2, int min_sum, int max_sum) {
         decreaseBy1(num1);
-        vector<vector<vector<int>>> dp(m + 1, vector<vector<int>>(221, vector<int>(2, -1)));
-        vector<vector<vector<int>>> dp2(m + 1, vector<vector<int>> (221, vector<int> (2, -1)));
-
-        int ans1 = 0;
-        for (int sum = sum1; sum <= min(220, sum2); sum++) {
-            ans1 = (ans1 + build(num1, 0, n, sum, true, dp)) % mod;
-        }
-        int ans2 = 0;
-        for (int sum = sum1; sum <= min(220, sum2); sum++) {
-            ans2 = (ans2 + build(num2, 0, m, sum, true, dp2)) % mod;
-        }
-
-        return ((ans2 - ans1) % mod + mod) % mod;
+        vector<vector<vector<int>>> dp(23, 
+            vector<vector<int>> (2, 
+                    vector<int> (220, -1)));
+        vector<vector<vector<int>>> dp2(23, 
+            vector<vector<int>> (2, 
+                    vector<int> (220, -1)));
+        return (solve(0, num2, 0, min_sum, max_sum, true, dp) - solve(0, num1, 0, min_sum, max_sum, true, dp2) + mod) % mod;
     }
 };
