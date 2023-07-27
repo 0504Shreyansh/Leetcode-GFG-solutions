5ab3fb6e3a6bc0b841e9003cfb526866e5d74a50
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-27 15:24:08 +0530

    Time: 217 ms (43.17%), Space: 55.8 MB (36.33%) - LeetHub

--- 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
+++ 2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cpp
@@ -1,16 +1,32 @@
 class Solution {
+private:
+    bool isPossible(vector<int>& batteries, int n, long long mid) {
+        long long cur = 0;
+        for (auto &it : batteries) {
+            if (it > mid) cur += mid;
+            else cur += it;
+        }
+        return (cur >= mid * n);
+    }
 public:
     long long maxRunTime(int n, vector<int>& batteries) {
+        long long low = 1;
+        long long high = 0;
         sort(begin(batteries),end(batteries));
-        long long sum = 0;
         for (auto &it : batteries) {
-            sum += it;
+            high += it;
         }
-        int i = batteries.size() - 1;
-        while (batteries[i] > sum / n) {
-            sum -= batteries[i--];
-            n--;
+        high /= n;
+        long long ans = -1;
+        while (low <= high) {
+            long long mid = (low + high) >> 1;
+            if (isPossible(batteries, n, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        return sum / n;
+        return ans;
     }
 };
