e24123225a1c35ad99b870a72a1965c589faf904
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-28 08:38:08 +0530

    Time: 6 ms (52.86%), Space: 9.5 MB (5.50%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,11 +1,12 @@
 class Solution {
 private:
-    int solve(int i, int j, vector<int>& nums, bool turn) {
+    int solve(int i, int j, vector<int>& nums, bool turn, vector<vector<vector<int>>>& dp) {
         if (i > j) return 0;
+        if (dp[i][j][turn] != -1) return dp[i][j][turn];
         if (turn) {
-            return max(nums[i] + solve(i + 1, j, nums, !turn), nums[j] + solve(i, j - 1, nums, !turn));
+            return dp[i][j][turn] = max(nums[i] + solve(i + 1, j, nums, !turn, dp), nums[j] + solve(i, j - 1, nums, !turn, dp));
         }
-        return min(solve(i + 1, j, nums, !turn), solve(i, j - 1, nums, !turn));
+        return dp[i][j][turn] = min(solve(i + 1, j, nums, !turn, dp), solve(i, j - 1, nums, !turn, dp));
     }
 public:
     bool PredictTheWinner(vector<int>& nums) {
@@ -13,7 +14,10 @@ public:
         for (auto &it : nums) {
             totalSum += it;
         }
-        int sum1 = solve(0, nums.size() - 1, nums, 1);
+        vector<vector<vector<int>>> dp(20, 
+                vector<vector<int>> (20, 
+                    vector<int> (2, -1)));
+        int sum1 = solve(0, nums.size() - 1, nums, 1, dp);
         int sum2 = totalSum - sum1;
         return sum1 >= sum2;
     }
