85906092df3313101ef53a07d762cc8be1f0c49b
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-07-31 06:14:16 +0530

    Time: 15 ms (98.12%), Space: 6.6 MB (99.06%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -7,18 +7,19 @@ public:
         int sum2 = 0;
         for (auto &it : s1) sum1 += it;
         for (auto &it : s2) sum2 += it;
-        vector<vector<int>> dp(N + 1, vector<int> (M + 1, 0));
+        vector<int> dp(M + 1, 0), prev(M + 1, 0);
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= M; j++) {
                 if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = int(s1[i - 1]) + dp[i - 1][j - 1];
+                    dp[j] = int(s1[i - 1]) + prev[j - 1];
                 } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[j] = max(prev[j], dp[j - 1]);
                 }
             }
+            prev = dp;
         }
-        int delCost1 = sum1 - dp[N][M];
-        int delCost2 = sum2 - dp[N][M];
+        int delCost1 = sum1 - dp[M];
+        int delCost2 = sum2 - dp[M];
         return delCost1 + delCost2;
     }
 };
