850c63313f23c13cf3a3e282f071b2a26b72ce5f
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-03 07:04:38 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (31.40%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,19 +1,25 @@
 class Solution {
 private:
     vector<string> res;
-    void solve(string cur, int i, string& digits, unordered_map<char, string>& map) {
+    void solve(string& cur, int i, string& digits, unordered_map<char, string>& map) {
         if (i >= digits.size()) {
             res.push_back(cur);
             return ;
         }
         for (auto &it : map[digits[i]]) {
-            solve(cur + it, i + 1, digits, map);
+            cur.push_back(it);
+            solve(cur, i + 1, digits, map);
+            cur.pop_back();
         }
     }
 public:
     vector<string> letterCombinations(string digits) {
-        unordered_map<char, string> map = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
-        solve("", 0, digits, map);
+        unordered_map<char, string> map = {{'2', "abc"}, {'3', "def"}, 
+                                           {'4', "ghi"}, {'5', "jkl"}, 
+                                           {'6', "mno"}, {'7', "pqrs"}, 
+                                           {'8', "tuv"}, {'9', "wxyz"}};
+        string cur = "";
+        solve(cur, 0, digits, map);
         if (res.back() == "") res.pop_back();
         return res;
     }
