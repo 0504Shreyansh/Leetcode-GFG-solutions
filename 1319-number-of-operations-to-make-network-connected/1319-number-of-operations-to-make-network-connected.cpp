f28dae016ee0778ac25ab58de8b7bcf9a48b5524
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-05 17:30:17 +0530

    Time: 101 ms (94.26%), Space: 50.7 MB (31.26%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,43 +1,28 @@
-class DisjointSet {
-    public: 
-    vector<int> rank, parent;
-    DisjointSet(int n) {
-        rank.resize(n+1);
-        parent.resize(n+1);
-        for(int i = 0; i <= n; i++) 
-            parent[i] = i;
-    }
-    int findParent(int node) {
-        return (node==parent[node]) ? node : parent[node]=findParent(parent[node]);
-    }
-    void unite(int u, int v) {
-        int up = findParent(u);
-        int vp = findParent(v);
-        if(up != vp) {
-            if(rank[up] < rank[vp]) 
-                parent[up] = vp;
-            else if(rank[up] > rank[vp])
-                parent[vp] = up;
-            else {
-                parent[up] = vp;
-                rank[vp]++;
+class Solution {
+private:
+    void dfs(int u, vector<int> adj[], vector<int>& vis) {
+        vis[u] = 1;
+        for (auto &v : adj[u]) {
+            if (!vis[v]) {
+                dfs(v, adj, vis);
             }
         }
     }
-};
-
-class Solution {
 public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        int res = 0;
-        DisjointSet ds(n);
-        for(auto it : connections) 
-            if(ds.findParent(it[0]) != ds.findParent(it[1])) 
-                ds.unite(it[0], it[1]);
-    
-        for(int i = 0; i < n; i++) 
-            res += (ds.parent[i]==i);
-        
-        return (connections.size() >= n-1) ? res-1 : -1;
+    int makeConnected(int n, vector<vector<int>>& C) {
+        vector<int> adj[n];
+        for (auto &it : C) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        int comp = 0;
+        vector<int> vis(n);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                comp++;
+                dfs(i, adj, vis);
+            }
+        }
+        return (C.size() >= n - 1) ? comp - 1 : -1;
     }
 };
