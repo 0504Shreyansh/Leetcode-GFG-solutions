2fdd242d1db40aa29c217d3516db6d0edfcdfe87
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-05 19:49:14 +0530

    Time: 141 ms (99.39%), Space: 60.6 MB (87.97%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,33 +1,35 @@
 class Solution {
-private:
-    bool bfs(int start, int dest, vector<int> adj[], int n) {
-        vector<int> vis(n);
-        vis[start] = 1;
-        queue<int> q({start});
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+        vector<int> adj[n];
+        vector<int> indegree(n);
+        for (auto &it : prerequisites) {
+            adj[it[0]].push_back(it[1]);
+            indegree[it[1]]++;
+        }
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+        vector<vector<bool>> table(n, vector<bool> (n, false));
         while (q.size()) {
             int u = q.front();
             q.pop();
-            if (u == dest) return 1;
             for (auto &v : adj[u]) {
-                if (!vis[v]) {
-                    vis[v] = 1;
+                table[u][v] = true;
+                for (int i = 0; i < n; i++) 
+                    if (table[i][u] == 1)
+                        table[i][v] = 1;
+                if (--indegree[v] == 0) {
                     q.push(v);
                 }
             }
         }
-        return 0;
-    }
-public:
-    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<int> adj[n];
-        for (auto &it : prerequisites) {
-            adj[it[0]].push_back(it[1]);
-        }
-        vector<bool> ans(queries.size(), 0);
-        for (int i = 0; i < queries.size(); i++) {
-            int u = queries[i][0], v = queries[i][1];
-            bool res = bfs(u, v, adj, n);
-            if (res) ans[i] = 1;
+        vector<bool> ans;
+        for (auto &it : queries) {
+            ans.push_back(table[it[0]][it[1]]);
         }
         return ans;
     }
