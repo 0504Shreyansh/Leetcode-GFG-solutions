aa505fd3c1e58f8d1a66a77c99081bb65110d608
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-06 21:39:09 +0530

    Time: 440 ms (99.75%), Space: 162.7 MB (71.61%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,53 +1,30 @@
-class DSU {
-public:
-    vector<int> rank;
-    vector<int> par;
-    DSU(int n) {
-        rank.resize(n);
-        par.resize(n);
-        for (int i = 0; i < n; i++) par[i] = i;
-    }
-    int findPar(int u) {
-        return (par[u] == u) ? u : par[u] = findPar(par[u]);
-    }
-    void unite(int u, int v) {
-        u = findPar(u);
-        v = findPar(v);
-        if (u != v)
-            par[u] = par[v] = min(u, v);
-    }
-};
-
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        sort(begin(meetings),end(meetings),[&](auto &a, auto &b) {
-            return a[2] < b[2];
-        });
-        int m = meetings.size();
-        DSU obj(n);
-        obj.unite(0, firstPerson);
-        for (int i = 0; i < m; i++) {
-            int time = meetings[i][2];
-            int index = i;
-            while (i < m && meetings[i][2] == time) {
-                int u = meetings[i][0], v = meetings[i][1];
-                obj.unite(u, v);
-                i++;
-            }
-            while (index < i) {
-                int u = meetings[index][0], v = meetings[index][1];
-                if (obj.findPar(u) != 0 && obj.findPar(v) != 0) {
-                    obj.par[u] = u;
-                    obj.par[v] = v;
+        vector<pair<int, int>> graph[n];
+        for (auto &it : meetings) {
+            graph[it[0]].push_back({it[1], it[2]});
+            graph[it[1]].push_back({it[0], it[2]});
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+        pq.push({0, firstPerson});
+        vector<int> vis(n);
+        while (pq.size()) {
+            auto [time, person] = pq.top();
+            pq.pop();
+            if (vis[person]) 
+                continue;
+            vis[person] = 1;
+            for (auto &[neigh, newTime] : graph[person]) {
+                if (!vis[neigh] && time <= newTime) {
+                    pq.push({newTime, neigh});
                 }
-                index++;
             }
-            i--;
         }
         vector<int> ans;
         for (int i = 0; i < n; i++) {
-            if (obj.findPar(i) == 0) {
+            if (vis[i]) {
                 ans.push_back(i);
             }
         }
