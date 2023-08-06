67beed3516ec8f88c17c9633bc769ba4005ea200
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-06 17:02:21 +0530

    Time: 454 ms (94.90%), Space: 158 MB (99.65%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -1,27 +1,37 @@
 class Solution {
-private:
-    int dfs(int node, int par, vector<int> adj[], int& seats, long long &ans) {
-        int cur = 1;
-        for (auto &adjNode : adj[node]) {
-            if (par != adjNode) {
-                cur += dfs(adjNode, node, adj, seats, ans);
-            }
-        }
-        if (node > 0) {
-            ans += (cur + seats - 1) / seats;
-        }
-        return cur;
-    }
 public:
     long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
         int n = roads.size() + 1;
         vector<int> adj[n];
+        vector<int> indegree(n);
         for (auto &it : roads) {
             adj[it[0]].push_back(it[1]);
             adj[it[1]].push_back(it[0]);
+            indegree[it[0]]++, indegree[it[1]]++;
         }
+        queue<int> q;
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 1) {
+                q.push(i);
+            }
+        }
+        vector<int>people(n, 1);
         long long ans = 0;
-        dfs(0, -1, adj, seats, ans);
+        while (q.size()) {
+            int size = q.size();
+            while (size--) {
+                int u = q.front();
+                q.pop();
+                if (u == 0) break;
+                ans += (people[u] + seats - 1) / seats;
+                for (int &v : adj[u]) {
+                    people[v] += people[u];
+                    if (--indegree[v] == 1) {
+                        q.push(v);
+                    }
+                }
+            }
+        }
         return ans;
     }
 };
