7f770d2916de1d654fb94623b36be31cad4a12fb
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-06 12:52:48 +0530

    Time: 299 ms (88.15%), Space: 160.1 MB (19.35%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        
-        //Available items
-        unordered_set<string> s;
-        for(auto item : supplies)
-            s.insert(item);      
-		
-        //Initialise the indegrees of recipes with 0
-        unordered_map<string,int> indegree;  
-        for(auto food : recipes)
-            indegree[food] = 0;  
-    
-        unordered_map<string,vector<string>> graph;
-        int n = recipes.size();
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < (int)ingredients[i].size(); j++) {
-            //ing[i][j] is not avilable in the supply list, so we can assume a directed edge.
-            //This means ing[i][j] is needed for making rec[i] (++inDegree[rec[i]])
-                if(s.find(ingredients[i][j]) == s.end()){     
-                    graph[ingredients[i][j]].push_back(recipes[i]); 
-                    indegree[recipes[i]]++;          
+        unordered_set<string> present(begin(supplies),end(supplies));
+        unordered_map<string, vector<string>> graph;
+        unordered_map<string, int> indegree;
+        for (int i = 0; i < recipes.size(); i++) {
+            indegree[recipes[i]] = 0;
+            for (auto &it : ingredients[i]) {
+                graph[it].push_back(recipes[i]);
+                if (!present.count(it)) {
+                    indegree[recipes[i]]++;
                 }
             }
         }
-        
-        //cnt==0 means these items can be made by the currently available supply list
-        queue<string> q;
-        for(auto [item,cnt] : indegree)
-            if(cnt==0)
-                q.push(item);
-        
-        vector<string> res;
-        while(!q.empty()){
-            string cur = q.front();
-            q.pop();
-            res.push_back(cur);
-            //inDegree cnt zero further signifies we have satisfied the needs of the current rec.
-            for(auto nbr : graph[cur])
-                if(--indegree[nbr]==0)
-                    q.push(nbr);
+        queue<string> Q;
+        for (auto &it : indegree) {
+            if (it.second == 0) {
+                Q.push(it.first);
+            }
         }
-        return res;
-        
+        vector<string> ans;
+        while (Q.size()) {
+            int size = Q.size();
+            while (size--) {
+                string u = Q.front();
+                Q.pop();
+                ans.push_back(u);
+                for (auto &v : graph[u]) {
+                    if (--indegree[v] == 0) {
+                        present.insert(v);
+                        Q.push(v);
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
