bbf902e6e5111a7a15fd9e117f9a56542696f0c6
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-06 11:52:12 +0530

    Time: 270 ms (38.53%), Space: 131.8 MB (30.62%) - LeetHub

---
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+    vector<int> bfs(int node, vector<int> adj[], int n) {
+        vector<int> dist(n, 1e9);
+        dist[node] = 0;
+        int steps = 0;
+        queue<int> Q({node});
+        while (Q.size()) {
+            int size = Q.size();
+            while (size--) {
+                int u = Q.front();
+                Q.pop();
+                for (auto &v : adj[u]) {
+                    if (dist[v] > dist[u] + 1) {
+                        dist[v] = dist[u] + 1;
+                        Q.push(v);
+                    }
+                }
+            }
+            steps++;
+        }
+        return dist;
+    }
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        int n = edges.size();
+        vector<int> adj[n];
+        for (int i = 0; i < n; i++) {
+            if (edges[i] != -1)
+                adj[i].push_back(edges[i]);
+        }
+        vector<int> dist1 = bfs(node1, adj, n);
+        vector<int> dist2 = bfs(node2, adj, n);
+        int d = 1e9;
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            if (dist1[i] != 1e9 && dist2[i] != 1e9) {
+                if (d > max(dist1[i], dist2[i])) {
+                    d = max(dist1[i], dist2[i]);
+                    ans = i;
+                }
+            }
+        }
+        return ans;
+    }
+};
