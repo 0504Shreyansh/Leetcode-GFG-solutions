5ff2f4b3eadb1bd7ca50b1fa3a51079132417f5d
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-06 18:52:59 +0530

    Time: 190 ms (99.73%), Space: 55.9 MB (16.65%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,29 +1,28 @@
 class Solution {
-public:
+private:
+    int dirs[5] = {0, -1, 0, 1, 0};
     const int mod = 1e9 + 7;
-    vector<pair<int,int>> direction = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
+        if (dp[i][j] != -1) return dp[i][j];
         int ans = 0;
-        for(auto [x, y] : direction) {
-            int X = i + x;
-            int Y = j + y;
-            if(X >= 0 && X < grid.size() && Y >= 0 && Y < grid[0].size() && grid[i][j] < grid[X][Y]) {
-                ans = (ans + 1 + dfs(X, Y, grid, dp)) % mod;
+        for (int k = 0; k < 4; k++) {
+            int x = i + dirs[k], y = j + dirs[k + 1];
+            if (x >= 0 && x < grid.size() && y >= 0 && y < grid[0].size() && grid[x][y] > grid[i][j]) {
+                ans += 1 + dfs(x, y, grid, dp);
+                ans %= mod;
             }
         }
         return dp[i][j] = ans;
     }
-
+public:
     int countPaths(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        int ans = 0;
         vector<vector<int>> dp(n, vector<int> (m, -1));
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                ans = (ans + 1 + dfs(i, j, grid, dp)) % mod;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                ans += 1 + dfs(i, j, grid, dp);
+                ans %= mod;
             }
         }
         return ans;
