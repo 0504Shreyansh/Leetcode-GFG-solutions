f1af67f41c0bdd8e2101ac370990eb94d97be36c
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-09 21:25:22 +0530

    Time: 12 ms (56.57%), Space: 21.3 MB (26.24%) - LeetHub

--- 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
+++ 0968-binary-tree-cameras/0968-binary-tree-cameras.cpp
@@ -10,36 +10,27 @@
  * };
  */
 class Solution {
-public:
-    /*
-    -1 --> immediately wants a camera
-    0 --> no condition
-    1 --> can provide with a camera
+private:
+    /* 
+    -1 --> need camera urgently
+    0 --> no urgent need
+    1 --> can provide camera facility
     */
-    int inorder(TreeNode* root, int &camera_count) {
-        // no condition for camera for NULL nodes
-        if(root == NULL) {
-            return 0;
-        }
-        int left = inorder(root->left, camera_count);
-        int right = inorder(root->right, camera_count);
-        // if any child immediately wants a camera --> install one.
-        if(left == -1 || right == -1) {
-            camera_count++;
+    int cameras = 0;
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+        int l = dfs(root -> left);
+        int r = dfs(root -> right);
+        if (l == -1 || r == -1) {
+            cameras++;
             return 1;
         }
-        // if any child can provide with a camera --> its ok!
-        else if(left == 1 || right == 1) {
-            return 0;
-        }
-        // ask parent for cameras
-        return -1;
+        if (l == 0 && r == 0) return -1;
+        return 0;
     }
+public:
     int minCameraCover(TreeNode* root) {
-        int camera_count = 0;
-        // if root node wants a camera --> give it one.
-        if(inorder(root, camera_count) == -1)
-            camera_count++;
-        return camera_count;
+        if (dfs(root) == -1) cameras++;
+        return cameras;
     }
 };
