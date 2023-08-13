9552a583ce97d8834c6965c78805c249b6ff639c
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-13 08:21:51 +0530

    Time: 129 ms (88.31%), Space: 92.9 MB (22.46%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,38 +1,18 @@
 class Solution {
-public:
-    int dp[100001];
-    bool solve(vector<int> &nums, int i=0) {
-        int n = nums.size();
-        if(i>=n) {
-            return true;
-        }
-
-        if(dp[i] != -1) {
-            return dp[i];
-        }
-
-        bool res = false;
-        // case 1
-        if((i+1) < n) {
-            if(nums[i] == nums[i+1]) res = res|solve(nums, i+2);
-        }
-
-        // case 2
-        if((i+2) < n) {
-            if(nums[i] == nums[i+1] && nums[i+1] == nums[i+2])
-                res = res | solve(nums, i+3);
-        }
-
-        // case 3
-        if((i+2) < n) {
-            if((nums[i+1]-nums[i]) == 1 && (nums[i+2]-nums[i+1]) == 1)
-                res = res | solve(nums, i+3);
-        }
-
-        return dp[i] = res;
+private:
+    bool solve(int i, vector<int>& A, int &n, vector<int>& dp) {
+        if (i >= n) return true;
+        if (dp[i] != -1) return dp[i];
+        dp[i] = false;
+        if (i + 1 < n && A[i] == A[i + 1]) dp[i] |= solve(i + 2, A, n, dp);
+        if (i + 2 < n && A[i] == A[i + 1] && A[i] == A[i + 2]) dp[i] |= solve(i + 3, A, n, dp);
+        if (i + 2 < n && A[i + 1] - A[i] == 1 && A[i + 2] - A[i + 1] == 1) dp[i] |= solve(i + 3, A, n, dp);
+        return dp[i];
     }
-    bool validPartition(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
-        return solve(nums);
+public:
+    bool validPartition(vector<int>& A) {
+        int n = A.size();
+        vector<int> dp(n, -1);
+        return solve(0, A, n, dp);
     }
 };
