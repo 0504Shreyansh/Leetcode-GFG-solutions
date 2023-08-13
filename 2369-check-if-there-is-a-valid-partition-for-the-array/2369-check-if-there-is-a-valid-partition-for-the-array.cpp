8a4553e45388d09864d5cfaf535b6e7fb2cfa1fd
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-13 11:39:35 +0530

    Time: 141 ms (77.54%), Space: 87.6 MB (79.08%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -4,14 +4,25 @@ public:
         int n = A.size();
         vector<int> dp(n + 1, false);
         dp[n] = true;
-        for (int i = n - 1; i >= 0; i--) {
-            if (i + 1 < n && A[i] == A[i + 1]) 
-                dp[i] |= dp[i + 2];
-            if (i + 2 < n && A[i] == A[i + 1] && A[i] == A[i + 2]) 
-                dp[i] |= dp[i + 3];
-            if (i + 2 < n && A[i + 1] - A[i] == 1 && A[i + 2] - A[i + 1] == 1)
-                dp[i] |= dp[i + 3];
+        bool S2 = 0, S3 = 1;
+        bool S1 = (A[n - 1] == A[n - 2]);
+        if (n == 2) return S1;
+        bool S0 = (A[n - 1] == A[n - 2] && A[n - 2] == A[n - 3])
+            || (A[n - 1] - 1 == A[n - 2] && A[n - 2] - 1 == A[n - 3]);
+        if (n == 3) return S0;
+        for (int i = n - 3; i >= 0; i--) {
+            if (A[i] == A[i + 1]) 
+                S0 |= S2;
+            if (A[i] == A[i + 1] && A[i] == A[i + 2]) 
+                S0 |= S3;
+            if (A[i + 1] - A[i] == 1 && A[i + 2] - A[i + 1] == 1)
+                S0 |= S3;
+            S3 = S2;
+            S2 = S1;
+            S1 = S0;
+            S0 = 0;
+            // cout << S0 << ' ' << S1 << ' ' << S2 << ' ' << S3 << endl;
         }
-        return dp[0];
+        return S1;
     }
 };
