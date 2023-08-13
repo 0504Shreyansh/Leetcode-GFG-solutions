bba397894da32ad7fcc6c2e575c07e45ca9f624f
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-13 08:28:58 +0530

    Time: 138 ms (79.08%), Space: 87.7 MB (77.23%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
-private:
-    bool solve(int i, vector<int>& A, int &n, vector<int>& dp) {
-        if (i >= n) return true;
-        if (dp[i] != -1) return dp[i];
-        dp[i] = false;
-        if (i + 1 < n && A[i] == A[i + 1]) dp[i] |= solve(i + 2, A, n, dp);
-        if (i + 2 < n && A[i] == A[i + 1] && A[i] == A[i + 2]) dp[i] |= solve(i + 3, A, n, dp);
-        if (i + 2 < n && A[i + 1] - A[i] == 1 && A[i + 2] - A[i + 1] == 1) dp[i] |= solve(i + 3, A, n, dp);
-        return dp[i];
-    }
 public:
     bool validPartition(vector<int>& A) {
         int n = A.size();
-        vector<int> dp(n, -1);
-        return solve(0, A, n, dp);
+        vector<int> dp(n + 1, false);
+        dp[n] = true;
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + 1 < n && A[i] == A[i + 1]) 
+                dp[i] |= dp[i + 2];
+            if (i + 2 < n && A[i] == A[i + 1] && A[i] == A[i + 2]) 
+                dp[i] |= dp[i + 3];
+            if (i + 2 < n && A[i + 1] - A[i] == 1 && A[i + 2] - A[i + 1] == 1)
+                dp[i] |= dp[i + 3];
+        }
+        return dp[0];
     }
 };
