1b1b9b648f53aff8c87de9072cf760785f01ecd5
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-13 11:40:05 +0530

    Time: 143 ms (76.61%), Space: 83.8 MB (92.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     bool validPartition(vector<int>& A) {
         int n = A.size();
-        vector<int> dp(n + 1, false);
-        dp[n] = true;
         bool S2 = 0, S3 = 1;
         bool S1 = (A[n - 1] == A[n - 2]);
         if (n == 2) return S1;
         bool S0 = (A[n - 1] == A[n - 2] && A[n - 2] == A[n - 3])
             || (A[n - 1] - 1 == A[n - 2] && A[n - 2] - 1 == A[n - 3]);
-        if (n == 3) return S0;
         for (int i = n - 3; i >= 0; i--) {
             if (A[i] == A[i + 1]) 
                 S0 |= S2;
@@ -21,7 +18,6 @@ public:
             S2 = S1;
             S1 = S0;
             S0 = 0;
-            // cout << S0 << ' ' << S1 << ' ' << S2 << ' ' << S3 << endl;
         }
         return S1;
     }
