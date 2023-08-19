f2a078444e0afd41b6b80c9f1188cb04a0c07997
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-19 10:07:08 +0530

    Added solution - LeetHub

--- Subarray with given sum - GFG/subarray-with-given-sum.cpp
+++ Subarray with given sum - GFG/subarray-with-given-sum.cpp
@@ -12,13 +12,15 @@ class Solution
     {
         int i = 0;
         long long sum = 0;
-        for(int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) {
             sum += arr[j];
-            while(sum > s && i < j) {
+            while (sum > s && i < j) {
                 sum -= arr[i];
                 i++;
             }
-            if(sum == s) return {i+1, j+1};
+            if (sum == s) {
+                return {i+1, j+1};
+            }
         }
         return {-1};
     }
