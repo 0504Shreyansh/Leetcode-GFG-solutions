38dc33a7df84cdadff81cd630854ab542d3af5fc
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-24 15:32:02 +0530

    Added solution - LeetHub

--- Multiply two strings - GFG/multiply-two-strings.cpp
+++ Multiply two strings - GFG/multiply-two-strings.cpp
@@ -11,29 +11,28 @@ class Solution{
         bool neg1 = 0, neg2 = 0;
         if (s[0] == '-') neg1 = 1, s.erase(s.begin());
         if (p[0] == '-') neg2 = 1, p.erase(p.begin());
-        while (s.size() && s[0] == '0') s.erase(s.begin());
-        while (p.size() && p[0] == '0') p.erase(p.begin());
         if (p.size() > s.size()) swap(s, p);
         int n = s.size(), m = p.size();
-        // cout <<s << ' ' << p << endl;
-        vector<int> ans(n + m, 0);
+        vector<int> A(n + m, 0);
         for (int j = m - 1; j >= 0; j--) {
             int carry = 0;
             for (int i = n - 1; i >= 0; i--) {
-                ans[i + j + 1] += (p[j] - '0') * (s[i] - '0') + carry;
-                carry = ans[i + 1 + j] / 10;
-                ans[i + j + 1] %= 10;
+                A[i + j + 1] += (s[i] - '0') * (p[j] - '0') + carry;
+                carry = A[i + j + 1] / 10;
+                A[i + j + 1] %= 10;
             }
-            if (carry) {
-                ans[j] += carry;
-            } 
+            if (carry != 0) {
+                A[j] += carry;
+            }
         }
         string res;
-        while (ans.size() && ans[0] == 0) {
-            ans.erase(ans.begin());
+        if (neg1 != neg2) {
+            res += '-';
         }
-        if (neg1 != neg2) res += '-';
-        for (auto &it : ans) {
+        while (A.size() && A[0] == 0) {
+            A.erase(A.begin());
+        }
+        for (auto &it : A) {
             res += it + '0';
         }
         return res;
