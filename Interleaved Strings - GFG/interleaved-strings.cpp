651e1a4ae1524e0a80eea4ac033b33fc9751f84e
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-25 07:46:30 +0530

    Added solution - LeetHub

--- Interleaved Strings - GFG/interleaved-strings.cpp
+++ Interleaved Strings - GFG/interleaved-strings.cpp
@@ -11,16 +11,16 @@ class Solution{
   public:
     /*You are required to complete this method */
     bool solve(int i, int j, string &A, string &B, string &C, vector<vector<int>>& dp) {
-        if (i < 0) return (B.substr(0, j + 1) == C.substr(0, j + 1));
-        if (j < 0) return (A.substr(0, i + 1) == C.substr(0, i + 1));
+        if (i == 0) return (B.substr(0, j) == C.substr(0, j));
+        if (j == 0) return (A.substr(0, i) == C.substr(0, i));
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
         bool ans = false;
-        if (A[i] == C[i + j + 1]) {
+        if (A[i - 1] == C[i + j - 1]) {
             ans |= solve(i - 1, j, A, B, C, dp);
         }
-        if (B[j] == C[i + j + 1]) {
+        if (B[j - 1] == C[i + j - 1]) {
             ans |= solve(i, j - 1, A, B, C, dp);
         }
         return dp[i][j] = ans;
@@ -29,8 +29,8 @@ class Solution{
     {
         int n = A.size(), m = B.size(), k = C.size();
         if (n + m != k) return false;
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return solve(n - 1, m - 1, A, B, C, dp);
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        return solve(n, m, A, B, C, dp);
     }
 
 };
