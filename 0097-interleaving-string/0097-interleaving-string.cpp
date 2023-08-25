54fcc53afccabf73e696dfe313cc1d53cf95d06e
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-25 07:47:42 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (44.21%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,32 +1,25 @@
 class Solution {
-public:
-    bool solve(int i, int j, int k, string s1, string s2, string s3, vector<vector<int>>& dp) {
-        
-        if(k==0) 
-            return true;
-        
-        if(i-1>=0 || j-1>=0) {
-            if(i-1>=0 && j-1>=0) {
-                if(dp[i][j] != -1) return dp[i][j];
-            } 
-            if(i-1>=0 && j-1>=0 && k-1>=0 && s1[i-1]==s3[k-1] && s2[j-1]==s3[k-1])
-                return dp[i][j] = solve(i-1, j, k-1, s1, s2, s3, dp) || 
-                    solve(i, j-1, k-1, s1, s2, s3, dp);
-            if(i-1>=0 && k-1>=0 && s1[i-1]==s3[k-1])
-                return dp[i][j] = solve(i-1, j, k-1, s1, s2, s3, dp);
-            if(j-1>=0 && k-1>=0 && s2[j-1]==s3[k-1])
-                return dp[i][j] = solve(i, j-1, k-1, s1, s2, s3, dp);
+private:
+    bool solve(int i, int j, string &A, string &B, string &C, vector<vector<int>>& dp) {
+        if (i == 0) return (B.substr(0, j) == C.substr(0, j));
+        if (j == 0) return (A.substr(0, i) == C.substr(0, i));
+        if (dp[i][j] != -1) {
+            return dp[i][j];
         }
-        
-        return dp[i][j] = false;
+        bool ans = false;
+        if (A[i - 1] == C[i + j - 1]) {
+            ans |= solve(i - 1, j, A, B, C, dp);
+        }
+        if (B[j - 1] == C[i + j - 1]) {
+            ans |= solve(i, j - 1, A, B, C, dp);
+        }
+        return dp[i][j] = ans;
     }
-    bool isInterleave(string s1, string s2, string s3) {
-       
-        int N = s1.size(), M = s2.size(), S = s3.size();
-        if(N + M != S)
-            return false;
-        vector<vector<int>> dp(N+1, vector<int> (M+1, -1));
-        return solve(N, M, S, s1, s2, s3, dp);
-        
+public:
+    bool isInterleave(string A, string B, string C)  {
+        int n = A.size(), m = B.size(), k = C.size();
+        if (n + m != k) return false;
+        vector<vector<int>> dp(n + 1, vector<int> (m + 1, -1));
+        return solve(n, m, A, B, C, dp);
     }
 };
