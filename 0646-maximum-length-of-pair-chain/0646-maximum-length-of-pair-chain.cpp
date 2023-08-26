7f7a1a2d6e1d3db58355b65af4d808409b4ec368
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-26 11:13:50 +0530

    Time: 66 ms (83.38%), Space: 26 MB (33.44%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -1,34 +1,21 @@
 class Solution {
-private:
-    int findNextIndex(int i, vector<vector<int>>& P) {
-        int low = i + 1, high = P.size() - 1;
-        int idx = high + 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (P[i][1] < P[mid][0]) {
-                idx = mid;
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return idx;
-    }
-    int solve(int i, vector<vector<int>>& P, vector<int>& dp) {
-        if (i >= P.size()) return 0;
-        if (dp[i] != -1) return dp[i];
-        int nextIdx = findNextIndex(i, P);
-        int notPick = solve(i + 1, P, dp);
-        int pick = 1 + solve(nextIdx, P, dp);
-        return dp[i] = max(pick, notPick);
-    }
 public:
-    int findLongestChain(vector<vector<int>> pairs) {
+    int findLongestChain(vector<vector<int>>& pairs) {
+        // Approach 1 --> DP + Binary Search
+        // Approach 2 --> Greedy (sort for ends)
         vector<vector<int>> P(pairs);
         sort(begin(P), end(P), [&](auto &a, auto &b) {
-            return a[0] < b[0];
+            return (a[1] == b[1]) ? a[0] < b[0] : a[1] < b[1];
         });
-        vector<int> dp(P.size(), -1);
-        return solve(0, P, dp);
+        int ans = 1;
+        int end = P[0][1];
+        int n = P.size();
+        for (int i = 1; i < n; i++) {
+            if (end < P[i][0]) {
+                end = P[i][1];
+                ans++;
+            }
+        }
+        return ans;
     }
 };
