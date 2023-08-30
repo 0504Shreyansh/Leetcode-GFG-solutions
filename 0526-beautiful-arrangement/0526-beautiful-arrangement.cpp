d3bc3d22be860e497731e368e53fc771680dfedc
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-30 19:06:24 +0530

    Time: 28 ms (79.27%), Space: 6 MB (82.11%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -1,26 +1,18 @@
 class Solution {
-public:
-    int size = 16;
-    vector<int> taken;
-    int solve(int i, int n) {
-
-        if(i==n) {
-            return 1;
-        }
-
-        int cur = 0;
-        for(int k = 1; k <= n; k++) {
-            if((k%(i+1)==0 || (i+1)%k==0) && !taken[k]) {
-                taken[k]++;
-                cur += solve(i+1, n);
-                taken[k]--;
+private:
+    int solve(int i, int mask, int n) {
+        if (i > n) return (mask == (1 << n) - 1);
+        int ans = 0;
+        for (int j = 1; j <= n; j++) {
+            if (mask & (1 << (j - 1))) continue;
+            if (i % j == 0 || j % i == 0) {
+                ans += solve(i + 1, mask | (1 << (j - 1)), n);
             }
         }
-        return cur;
+        return ans;
     }
-
+public:
     int countArrangement(int n) {
-        taken.resize(16);
-        return solve(0, n);
+        return solve(1, 0, n);
     }
 };
