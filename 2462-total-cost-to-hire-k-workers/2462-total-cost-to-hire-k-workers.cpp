1b959283cd67394cd6e0c1274fe8c2db2875883a
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-30 19:00:36 +0530

    Time: 163 ms (61.83%), Space: 75.7 MB (24.16%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -2,37 +2,28 @@ class Solution {
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
         int n = costs.size();
+        long long totCost = 0;
+        priority_queue<pair<int, int>> pq;
         int low = candidates - 1;
         int high = max(low + 1, n - candidates);
-        priority_queue<pair<int,int>> pq;
-        
-        // Consider first and last 'candidates' workers
-        // Be careful to not cross each other pointers
-        for(int i = 0; i <= low; i++) {
-            pq.push({-costs[i], -i});
-        }   
-        for(int i = n - 1; i >= high; i--) {
+        for (int i = 0; i <= low; i++) {
             pq.push({-costs[i], -i});
         }
-        
-        long long total_cost = 0;
-        while(pq.size() && k--) {
-            auto curr = pq.top();
+        for (int i = n - 1; i >= high; i--) {
+            pq.push({-costs[i], -i});
+        }
+        while (k--) {
+            auto cur = pq.top();
             pq.pop();
-            int curr_cost = -curr.first;
-            int index = -curr.second;
-            total_cost += curr_cost;
-            
-            // Adjust the window by considering the corresponding next index
-            // (if possible)
-            if(index >= high) {
-                high--;
-                if(low < high) pq.push({-costs[high], -high});
+            int cost = -cur.first;
+            int idx = -cur.second;
+            totCost += cost;
+            if (idx >= high) {
+                if (low < --high) pq.push({-costs[high], -high});
             } else {
-                low++;
-                if(low < high) pq.push({-costs[low], -low});
+                if (++low < high) pq.push({-costs[low], -low});
             }
         }
-        return total_cost;
+        return totCost;
     }
 };
