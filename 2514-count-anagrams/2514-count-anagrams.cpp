744cf6655cb13aad8ac3d856e23af445edf29b18
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-30 19:29:27 +0530

    Time: 98 ms (42.12%), Space: 61.6 MB (6.41%) - LeetHub

--- 2514-count-anagrams/2514-count-anagrams.cpp
+++ 2514-count-anagrams/2514-count-anagrams.cpp
@@ -1,46 +1,37 @@
 class Solution {
-public:
+private:
     const int mod = 1e9 + 7;
-    #define ll long long
-    ll fact[1000001];
-    // Calculates the factorial of numbers up to 1e5
-    void pre_compute() {
-        fact[0] = fact[1] = 1;
-        for(int i = 2; i < 1000001; i++)
-            fact[i] = (i * fact[i - 1]) % mod;
-    }
-    // Calculates a^b in log(b) time
-    ll binpow(ll a, ll b) {
-        ll ans = 1;
-        while(b) {
-            if(b & 1)
-                ans = (ans * a) % mod;
+    long long power(long long a, long long b) {
+        long long ans = 1;
+        while (b) {
+            if (b & 1) ans = (ans * a) % mod;
             b >>= 1;
             a = (a * a) % mod;
         }
         return ans;
     }
+public:
     int countAnagrams(string s) {
-        pre_compute();
-        stringstream ss(s);
-        int ans = 1;
-        while(ss >> s) {    // take out all the words
-            unordered_map<char,int> freq;
-            for(auto &it : s)
-                freq[it]++;
-            ll curr = fact[s.size()];
-            for(auto &it : freq) {
-                curr = (curr * binpow(fact[it.second], mod - 2)) % mod;
-            }
-            ans = (ans * curr) % mod;
-            
+        vector<long long> fact(100001, 1);
+        for (int i = 1; i <= 100000; i++) {
+            fact[i] = (i * fact[i - 1]) % mod;
         }
-        return ans;
+        stringstream ss(s);
+        string word;
+        long long res = 1;
+        while (ss >> word) {
+            unordered_map<char, int> mp;
+            for (auto &j : word) {
+                mp[j]++;
+            }
+            long long cur = fact[word.size()];
+            for (auto &j : mp) {
+                cur *= power(fact[j.second], mod - 2);
+                cur %= mod;
+            }
+            res *= cur;
+            res %= mod;
+        }
+        return res;
     }
 };
-
-/*
-shreyansh --> 9! / (2! * 2!)    n = 9, repeat: s(2), h(2).
-
-a / b % mod--> a * b^(-1) % mod--> a * b^(-1) % mod --> a * power(b, mod - 2)
-*/
