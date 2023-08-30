0b7f0fa801e9b690882e6c993f17f730081edbb8
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-08-30 11:57:02 +0530

    Time: 28 ms (96.17%), Space: 16.4 MB (35.97%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,34 +1,27 @@
 class Solution {
-public:
-    vector<pair<int,int>> direction = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& vis, vector<vector<int>>& dp) {
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        vis[i][j] = 1;
-        int cur = 1;
-        for(auto [dx, dy] : direction) {
-            int newX = i + dx;
-            int newY = j + dy;
-            if(newX >= 0 && newX < grid.size() && newY >= 0 && newY < grid[0].size() && !vis[newX][newY] && grid[i][j] < grid[newX][newY]) {
-                cur = max(cur, 1 + dfs(newX, newY, grid, vis, dp));
+private:
+    int dirs[5] = {0, -1, 0, 1, 0};
+    int dfs(int i, int j, vector<vector<int>>& grid, vector<vector<int>>& dp) {
+        if (dp[i][j] != -1) return dp[i][j];
+        int cnt = 0;
+        for (int k = 0; k < 4; k++) {
+            int X = i + dirs[k], Y = j + dirs[k + 1];
+            if (0 <= X && X < grid.size() && 0 <= Y && Y < grid[0].size() && grid[i][j] < grid[X][Y]) {
+                cnt = max(cnt, 1 + dfs(X, Y, grid, dp));
             }
         }
-        vis[i][j] = 0;
-        return dp[i][j] = cur;
+        return dp[i][j] = cnt;
     }
+public:
     int longestIncreasingPath(vector<vector<int>>& grid) {
-        int N = grid.size();
-        int M = grid[0].size();
-        // dp[i][j] -> max. increasing path length which starts from cell (i, j)
-        vector<vector<int>> dp(N + 1, vector<int> (M + 1, -1));
-        vector<vector<int>> vis(N, vector<int> (M));
-        int ans = 1;
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
-                ans = max(ans, dfs(i, j, grid, vis, dp));
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        int res = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res = max(res, 1 + dfs(i, j, grid, dp));
             }
         }
-        return ans;
+        return res;
     }
 };
