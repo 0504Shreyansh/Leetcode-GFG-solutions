8539b5c8f2b2f2d29037f3516531aa17ab2dc85e
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-09-02 07:40:10 +0530

    Time: 167 ms (59.17%), Space: 63.1 MB (86.05%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -17,7 +17,20 @@ public:
     int minExtraChar(string s, vector<string>& dictionary) {
         set<string> available(begin(dictionary), end(dictionary));
         int N = s.size();
-        vector<int> dp(N, -1);
-        return solve(0, N, s, available, dp);
+        vector<int> dp(N + 1);
+        // return solve(0, N, s, available, dp);
+        dp[N] = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            string c;
+            int ans = 1 + dp[i + 1];
+            for (int j = i; j < N; j++) {
+                c += s[j];
+                if (available.count(c)) {
+                    ans = min(ans, dp[j + 1]);
+                }
+            }
+            dp[i] = ans;
+        }
+        return dp[0];
     }
 };
