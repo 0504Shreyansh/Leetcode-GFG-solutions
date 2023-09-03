6dae0345b567d134bb313731691f860ad7be1a74
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-09-03 07:30:55 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (71.42%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        
-        vector<vector<long>> dp(m+1, vector<long> (n+1, 1));
-        
-        // Starting from cell (1, 1) bcoz possibility of reaching to row or col 1 is 1.
-        // Kind of base case
-        for(int i = 1; i <= m; i++) {
-            for(int j = 1; j <= n; j++) {
-                // At cell (i, j) => it can have possibility from the upper or the left cell, so add their possiblities
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
-            }
+        long double res = 1.0;
+        int r = m - 1;
+        for (int i = 1; i <= r; ++i) {
+            res *= (n + r - i);
+            res /= i;
         }
-        
-        return dp[m-1][n-1];
+        return res;
     }
 };
