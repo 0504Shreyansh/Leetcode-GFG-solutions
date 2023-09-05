c0d105a5952f896536c5352ea42738eb8406cd63
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-09-05 19:56:41 +0530

    Time: 2 ms (55.45%), Space: 8.7 MB (44.59%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -1,50 +1,42 @@
 class Solution {
 private:
-    double dijkstra(string u, string v, unordered_map<string, vector<pair<string, double>>>& graph, unordered_map<string, double> dist) {
-        if (graph.find(u) == graph.end() || graph.find(v) == graph.end()) {
-            return -1.0;
-        }
-        dist[u] = 1.0;
-        priority_queue<pair<double, string>> q;
-        q.push({-dist[u], u});
-        unordered_map<string, int> vis;
-        while (q.size()) {
-            auto cur = q.top();
-            q.pop();
-            double wt = -cur.first;
-            string a = cur.second;
-            if (vis[a]) continue;
-            vis[a] = 1;
-            for (auto &nbr : graph[a]) {
-                string b = nbr.first;
-                double w = nbr.second;
-                dist[b] = min(dist[b], dist[a] * w);
-                q.push({-dist[b], b});
+    double dijkstra(string &src, string &dest, map<string, vector<pair<string, double>>>& G) {
+        if (!G.count(src) || !G.count(dest)) return -1.0;
+        if (src == dest) return (G.count(src) != 0) ? 1.0 : -1.0;
+        map<string, double> dist;
+        dist[src] = 1;
+        priority_queue<pair<double, string>> pq;
+        pq.push({0, src});
+        while (!pq.empty()) {
+            string u = pq.top().second;
+            pq.pop();
+            if (u == dest) return dist[dest];
+            for (auto &[v, w] : G[u]) {
+                if (!dist.count(v)) {
+                    dist[v] = dist[u] * w;
+                    pq.push({-dist[v], v});
+                }
             }
         }
-        if (dist[v] < 1e-30) return 1.0;
-        return dist[v] == 2e9 * 1.0 ? -1 : dist[v];
+        return -1.0;
     }
 public:
-    vector<double> calcEquation(vector<vector<string>>& e, vector<double>& w, vector<vector<string>>& queries) {
-        int n = e.size();
-        unordered_map<string, vector<pair<string, double>>> graph;
-        unordered_map<string, double> dist;
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        int n = values.size();
+        map<string, vector<pair<string, double>>> G;
         for (int i = 0; i < n; i++) {
-            string u = e[i][0];
-            string v = e[i][1];
-            double wt = w[i];
-            graph[u].push_back({v, wt});
-            graph[v].push_back({u, 1.0/wt});
-            dist[u] = dist[v] = 2e9 * 1.0;
+            string u = equations[i][0];
+            string v = equations[i][1];
+            double w = values[i];
+            G[u].push_back({v, w});
+            G[v].push_back({u, 1.0 / w});
         }
-        vector<double> res;
+        vector<double> ans;
         for (auto &q : queries) {
-            string source = q[0];
-            string dest = q[1];
-            double wt = dijkstra(source, dest, graph, dist);
-            res.push_back(wt);
+            string src = q[0], dest = q[1];
+            double dist = dijkstra(src, dest, G);
+            ans.push_back(dist);
         }
-        return res;
+        return ans;
     }
 };
