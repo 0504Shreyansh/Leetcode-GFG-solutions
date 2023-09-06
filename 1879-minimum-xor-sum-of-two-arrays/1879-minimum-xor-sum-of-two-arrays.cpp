44cc38b5698a4caea6eb7cadc298ec3c6c3171e6
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-09-06 19:57:06 +0530

    Time: 26 ms (44.92%), Space: 18 MB (30.45%) - LeetHub

--- 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
+++ 1879-minimum-xor-sum-of-two-arrays/1879-minimum-xor-sum-of-two-arrays.cpp
@@ -1,24 +1,19 @@
 class Solution {
-public:
-    int solve(int i, int mask, vector<int>& nums1, vector<int>& nums2, vector<int>& dp) {
-        int n = nums1.size();
-        if(i >= n) {
-            return 0;
+private:
+    int solve(int i, int n, int mask, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp) {
+        if (i >= n) return 0;
+        if (dp[i][mask] != -1) return dp[i][mask];
+        int ans = 1e9;
+        for (int j = 0; j < n; j++) {
+            if (mask & (1 << j)) continue;
+            ans = min(ans, (nums1[i] ^ nums2[j]) + solve(i + 1, n, mask | (1 << j), nums1, nums2, dp));
         }
-        if(dp[mask] != -1) {
-            return dp[mask];
-        }
-        int ans = INT_MAX;
-        for(int j = 0; j < n; ++j) {
-            if(((mask >> j) & 1) == 0) {
-                int curr = (nums1[i] ^ nums2[j]) + solve(i + 1, mask | (1 << j), nums1, nums2, dp);
-                ans = min(ans, curr);
-            }
-        }
-        return dp[mask] = ans;
+        return dp[i][mask] = ans;
     }
+public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> dp(1 << nums1.size(), -1);
-        return solve(0, 0, nums1, nums2, dp);
+        int n = nums1.size();
+        vector<vector<int>> dp(n, vector<int>(1<<n, -1));
+        return solve(0, n, 0, nums1, nums2, dp);
     }
 };
