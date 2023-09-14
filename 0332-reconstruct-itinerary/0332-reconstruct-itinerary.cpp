37e4cf966da03a2fd5f75316eec3fdeaa6bf18ba
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-09-14 06:15:11 +0530

    Time: 41 ms (13.00%), Space: 16.3 MB (15.95%) - LeetHub

--- 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -6,23 +6,22 @@ private:
                 visit[{u, i}] = 1;
                 dfs(G[u][i], G, visit, st);
             }
-        }
+        } 
         st.push(u);
     }
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
         unordered_map<string, vector<string>> G;
-        for (auto &it : tickets) {
+        for (auto &it : tickets) 
             G[it[0]].push_back(it[1]);
-        }
-        for (auto &it : G) {
+        for (auto &it : G) 
             sort(begin(it.second), end(it.second));
-        }
+        
         map<pair<string, int>, bool> visit;
         stack<string> st;
         dfs("JFK", G, visit, st);
         vector<string> answer;
-        while (st.size()) {
+        while (!st.empty()) {
             answer.push_back(st.top());
             st.pop();
         }
