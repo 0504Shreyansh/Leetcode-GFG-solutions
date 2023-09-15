480ab6e75d802092ccb3a037df07d8f590ab5dc3
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-09-15 07:24:16 +0530

    Time: 1438 ms (15.06%), Space: 176.3 MB (21.03%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,51 +1,52 @@
 class DSU {
-    vector<int> rank;
-    vector<int> par;
+private:
+    vector<int> rank, par;
 public:
     DSU(int n) {
         rank.resize(n);
-        par.resize(n);
-        for (int i = 0; i < n; ++i) par[i] = i;
+        for (int i = 0; i < n; i++) 
+            par.push_back(i);
     }
-    int findPar(int u) {
-        return (par[u] == u) ? u : par[u] = findPar(par[u]);
+    int find(int u) {
+        return (par[u] == u) ? u : par[u] = find(par[u]);
     }
-    void unite(int u, int v) {
-        u = findPar(u);
-        v = findPar(v);
-        if (u != v) {
-            if (rank[u] < rank[v]) {
-                par[u] = v;
-            } else if (rank[u] > rank[v]) {
-                par[v] = u;
-            } else {
-                par[u] = v;
-                rank[v]++;
-            }
+    bool unite(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) return false;
+        if (rank[u] < rank[v]) {
+            par[u] = v; 
+        } else if (rank[u] > rank[v]) {
+            par[v] = u;
+        } else {
+            par[u] = v;
+            rank[v]++;
         }
+        return true;
     }
 };
 
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<int>> p;
+        vector<vector<int>> P;
         int n = points.size();
         for (int i = 0; i < n; i++) {
+            int x1 = points[i][0], y1 = points[i][1];
             for (int j = i + 1; j < n; j++) {
-                int dist = abs(points[j][0] - points[i][0]) + abs(points[j][1] - points[i][1]);
-                p.push_back({dist, i, j});
+                int x2 = points[j][0], y2 = points[j][1];
+                int dist = abs(x1 - x2) + abs(y1 - y2);
+                P.push_back({dist, i, j}); 
             }
         }
-        sort(begin(p),end(p));
+        int cost = 0;
+        sort(begin(P), end(P));
         DSU obj(n);
-        int ans = 0;
-        for (auto &it : p) {
-            if (obj.findPar(it[1]) != obj.findPar(it[2])) {
-                ans += it[0];
+        for (auto &it : P) {
+            int dist = it[0], u = it[1], v = it[2];
+            if (obj.unite(u, v)) {
+                  cost += dist;
             }
-            obj.unite(it[1], it[2]);
         }
-        return ans;
+        return cost;
     }
 };
