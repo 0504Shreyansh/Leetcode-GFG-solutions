1ed46cbae06bc5d45f0ce5fefe58b70dc610547d
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-10-06 07:00:44 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (36.38%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,27 +1,17 @@
 class Solution {
-public:
+private:
     int dp[60][60];
     int solve(int n, int k) {
-        if(k==0) {
-            return n;
-        }
-        if(dp[n][k] != -1) {
-            return dp[n][k];
-        }
-        int cur = 1;
-        for(int num = 1; num < n; num++) {
-            cur = max(cur, num*solve(n-num, k-1));
-        }
-        return dp[n][k] = cur;
-    }
-
-    int integerBreak(int n) {
-        memset(dp, -1, sizeof(dp));
+        if (n == 0) return (k >= 2) ? 1 : 0;
+        if (dp[n][k] != -1) return dp[n][k];
         int ans = 1;
-        // Try out all possible partitions.
-        for(int k = 1; k < n; k++) {
-            ans = max(ans, solve(n, k));
-        }
-        return ans;
+        for (int i = 1; i <= n; i++)
+            ans = max(ans, i * solve(n - i, k + 1));
+        return dp[n][k] = ans;
+    }
+public:
+    int integerBreak(int n) {
+        memset(dp, -1, sizeof dp);
+        return solve(n, 0);
     }
 };
