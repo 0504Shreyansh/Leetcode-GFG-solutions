ee047f270661f00a5e0143c7d5576c5a2d234421
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-10-09 14:34:40 +0530

    Time: 5 ms (63.32%), Space: 13.9 MB (25.30%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,32 +1,10 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& A, int k) {
-        int n = A.size();
-        vector<int> ans(2, -1);
-        int low = 0, high = n - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (A[mid] == k) {
-                ans[0] = mid;
-            } 
-            if (A[mid] < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        low = 0, high = n - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (A[mid] == k) {
-                ans[1] = mid;
-            } 
-            if (A[mid] <= k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        return ans;
+        int lidx = lower_bound(begin(A), end(A), k) - begin(A);
+        if (!(lidx >= 0 && lidx < A.size() && A[lidx] == k)) 
+            return {-1, -1};
+        int uidx = upper_bound(begin(A), end(A), k) - begin(A);
+        return {lidx, uidx - 1};
     }
 };
