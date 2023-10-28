25d6139c60a03ea05de784bf3d094dc7b16e5003
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-10-28 07:44:07 +0530

    Time: 55 ms (47.20%), Space: 27.6 MB (29.12%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,35 +1,16 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
     int countVowelPermutation(int n) {
-        vector<vector<int>> dp(n + 1, vector<int> (26, -1));
-        // dp[0][25]
-
-        for (int i = 0; i < 26; i++) {
-            dp[n][i] = 1;
+        const int mod = 1e9 + 7;
+        vector<vector<long long>> dp(n + 1, vector<long long> (6, 1));
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][0] = dp[i + 1][1];
+            dp[i][1] = (dp[i + 1][0] + dp[i + 1][2]) % mod;     
+            dp[i][2] = (dp[i + 1][0] + dp[i + 1][1] + dp[i + 1][3] + dp[i + 1][4]) % mod;
+            dp[i][3] = (dp[i + 1][2] + dp[i + 1][4]) % mod;
+            dp[i][4] = dp[i + 1][0];
+            dp[i][5] = (dp[i + 1][0] + dp[i + 1][1] + dp[i + 1][2] + dp[i + 1][3] + dp[i + 1][4]) % mod;
         }
-        for (int i = n -  1; i >= 0; i--) {
-            for (char prev = 'z'; prev >= 'a'; prev--) {
-                if (prev == 'z') {
-                    dp[i][prev - 'a'] = ((((dp[i + 1][0] + dp[i + 1][4]) % mod + dp[i + 1][8]) % mod + dp[i + 1][14]) % mod + dp[i + 1][20]) % mod;
-                }
-                else if (prev == 'a') {
-                    dp[i][prev - 'a'] = dp[i + 1][4];
-                }
-                else if (prev == 'e') {
-                    dp[i][prev - 'a'] = (dp[i + 1][0] + dp[i + 1][8]) % mod;
-                }
-                else if (prev == 'i') {
-                    dp[i][prev - 'a'] = (((dp[i + 1][0] + dp[i + 1][4]) % mod + dp[i + 1][14]) % mod + dp[i + 1][20]) % mod;
-                }
-                else if (prev == 'o') {
-                    dp[i][prev - 'a'] = (dp[i + 1][8] + dp[i + 1][20]) % mod;
-                } 
-                else if (prev == 'u') {
-                    dp[i][prev - 'a'] = dp[i + 1][0];
-                }
-            }
-        }
-        return dp[0][25];
+        return dp[0][5];
     }
 };
