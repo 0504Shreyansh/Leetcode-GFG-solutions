91921061a0740e4f1890647826b14fed803bbe2a
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-10-28 07:47:47 +0530

    Time: 15 ms (69.44%), Space: 6.2 MB (89.44%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -2,15 +2,16 @@ class Solution {
 public:
     int countVowelPermutation(int n) {
         const int mod = 1e9 + 7;
-        vector<vector<long long>> dp(n + 1, vector<long long> (6, 1));
+        vector<long long> dp(6, 1), cur(6, 1);
         for (int i = n - 1; i >= 0; i--) {
-            dp[i][0] = dp[i + 1][1];
-            dp[i][1] = (dp[i + 1][0] + dp[i + 1][2]) % mod;     
-            dp[i][2] = (dp[i + 1][0] + dp[i + 1][1] + dp[i + 1][3] + dp[i + 1][4]) % mod;
-            dp[i][3] = (dp[i + 1][2] + dp[i + 1][4]) % mod;
-            dp[i][4] = dp[i + 1][0];
-            dp[i][5] = (dp[i + 1][0] + dp[i + 1][1] + dp[i + 1][2] + dp[i + 1][3] + dp[i + 1][4]) % mod;
+            cur[0] = dp[1];
+            cur[1] = (dp[0] + dp[2]) % mod;     
+            cur[2] = (dp[0] + dp[1] + dp[3] + dp[4]) % mod;
+            cur[3] = (dp[2] + dp[4]) % mod;
+            cur[4] = dp[0];
+            cur[5] = (dp[0] + dp[1] + dp[2] + dp[3] + dp[4]) % mod;
+            dp = cur;
         }
-        return dp[0][5];
+        return dp[5];
     }
 };
