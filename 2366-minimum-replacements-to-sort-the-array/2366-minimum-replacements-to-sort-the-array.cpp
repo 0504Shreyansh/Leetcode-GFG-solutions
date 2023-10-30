5f27323e0bdb7ffdec6c3abc1f4dce28f4f491cb
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-10-30 21:07:17 +0530

    Time: 97 ms (24.39%), Space: 55 MB (11.80%) - LeetHub

--- 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
+++ 2366-minimum-replacements-to-sort-the-array/2366-minimum-replacements-to-sort-the-array.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
-    long long minimumReplacement(vector<int>& A) {
-        //Consider splitting the last ele --> bad option
-        //So consider it to be the largest ele in the sorted list
+    long long minimumReplacement(vector<int>& arr) {
+        int n = arr.size();
         long long res = 0;
-        int n = A.size();
-        int prevLargest = A.back();
+        int prev = arr.back();
         for (int i = n - 2; i >= 0; i--) {
-            //Most optimal way is to split in al most equals i.e.
-            //Consider case1 - _,_,24,6
-            //_,_,6,6,6,6,6 => ans += 4 (ceil(24/6)) - 1
-            //Consider case2 - _,_28,6
-            //_,_,4,6,6,6,6,6 => ans += 5 (ceil(28/6)) - 1
-            //So, splits = ceil(nums[i]/prevLargest)
-            if(A[i] > prevLargest) {
-                int splits = (A[i] - 1) / prevLargest;
-                //splits-1 bcoz one was the number itself
-                res += splits;
-                prevLargest = A[i] / (splits + 1);
-            } else { 
-                //if an ele is found smaller than var, then update it.
-                prevLargest = A[i];
+            if (arr[i] > prev) {
+                int cur = ((arr[i] - 1) / prev);
+                res += cur;
+                prev = arr[i] / (cur + 1);
+            } else {
+                prev = arr[i];
             }
         }
         return res;
