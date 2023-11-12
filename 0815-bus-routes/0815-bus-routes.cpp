d242b8afd5b8b772719a368868d4b3bd1ebfee98
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-11-12 14:19:43 +0530

    Time: 629 ms (37.08%), Space: 57.6 MB (77.65%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.cpp
+++ 0815-bus-routes/0815-bus-routes.cpp
@@ -10,23 +10,25 @@ public:
         }
         
         unordered_set<int> seen_route;
-        queue<vector<int>> q({{source, 0}});
+        queue<int> q({source});
+        int route_count = 0; 
         while (!q.empty()) {
             int size = q.size();
             while (size--) {
-                int bus = q.front()[0], cnt = q.front()[1];
+                int bus = q.front();
                 q.pop();
                 if (bus == target) 
-                    return cnt;
+                    return route_count;
                 for (int &route : graph[bus]) {
                     if (seen_route.find(route) != seen_route.end())
                         continue;
                     seen_route.insert(route);
                     for (int &currBus : routes[route]) {
-                        q.push({currBus, cnt + 1});
+                        q.push(currBus);
                     }
                 }
             }
+            route_count++;
         }
         return -1;
     }
