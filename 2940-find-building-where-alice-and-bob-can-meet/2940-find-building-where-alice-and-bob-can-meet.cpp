90ac1cd50c7fcdb2f88c47d07b6c55c781ad6839
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-11-19 15:06:52 +0530

    Time: 344 ms (82.35%), Space: 176.4 MB (23.53%) - LeetHub

---
+++ 2940-find-building-where-alice-and-bob-can-meet/2940-find-building-where-alice-and-bob-can-meet.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        stack<int> st;
+        int n = heights.size();
+        vector<int> nextGreater(n, -1);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] <= heights[i]) 
+                st.pop();
+            if (!st.empty())
+                nextGreater[i] = st.top();
+            st.push(i);
+        }
+        
+        vector<int> res;
+        for (auto &q : queries) {
+            int idx1 = nextGreater[q[0]], idx2 = nextGreater[q[1]];
+            if (q[0] == q[1]) {
+                res.push_back(q[0]);
+            } else if (q[0] < q[1]) {
+                 if (heights[q[0]] < heights[q[1]]) {
+                    res.push_back(q[1]);
+                } else {
+                    if (idx1 != -1 && idx2 != -1) {
+                        int idx = max(idx1, idx2);
+                        while (idx != -1 && heights[idx] < heights[q[0]]) {
+                            idx = nextGreater[idx];
+                        }
+                        res.push_back(idx);
+                    } else {
+                        res.push_back(-1);
+                    }
+                }
+            } else {
+                 if (heights[q[0]] <= heights[q[1]]) {
+                    if (idx1 != -1 && idx2 != -1) {
+                        int idx = max(idx1, idx2);
+                        while (idx != -1 && heights[idx] < heights[q[1]]) {
+                            idx = nextGreater[idx];
+                        }
+                        res.push_back(idx);
+                    } else {
+                        res.push_back(-1);
+                    }
+                } else {
+                    res.push_back(q[0]);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+/*
+[2,5,-1,5,2]
+[7,6,-1,4,6]
+[0,1,-1,-1,1,1,-1,-1,-1,-1,2,3,-1,-1,3,3]
+[0,1,3,3,5,5,1,1,-1,-1,-1,-1,3,-1,2,3,5,5,3,-1,3,3,-1,-1,5,-1,5,-1,4,5,5,-1,5,-1,5,5]
+*/
