d316dd26cb90a328dc4fc09dad7459dbd2b3d604
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2023-11-27 19:41:56 +0530

    Time: 156 ms (48.68%), Space: 24 MB (60.67%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,37 +1,22 @@
 class Solution {
-public:
-    // int store[10][5001];
-    int solverrr(int i, vector<vector<int>>& positions, int n, vector<vector<int>>& store) {
-        if(n==1) {
-            return 1;
-        }
-
-        if(store[i][n] != -1) {
-            return store[i][n];
-        }
-        
-        int cur = 0;
-        for(auto pos : positions[i]) {
-            cur = (cur + solverrr(pos, positions, n-1, store)) % 1000000007;    
-        }
-        
-        return store[i][n] = cur % 1000000007;
+private:
+    const int mod = 1e9 + 7;
+    vector<vector<int>> next = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {3, 9, 0}, 
+                                     {}, {1, 7, 0}, {2, 6}, {1, 3}, {2, 4}};
+    int solve(int digit, int n, vector<vector<int>>& dp) {
+        if (n == 0) return 1;
+        if (dp[digit][n] != -1) return dp[digit][n];
+        dp[digit][n] = 0;
+        for (int &d : next[digit]) 
+            (dp[digit][n] += solve(d, n - 1, dp)) %= mod;
+        return dp[digit][n];
     }
-
+public:
     int knightDialer(int n) {
-        
-        // Store the possible positions of each number in the dial pad
-        vector<vector<int>> positions = {{4, 6}, {6, 8}, {7, 9}, {4, 8}, {0, 3, 9}, {}, {0, 1, 7}, {2, 6}, {1, 3}, {2, 4}};
-
-        // Create a dp array of 10*n dimension
-        vector<vector<int>> store(10, vector<int> (n+1, -1));
-
-        // Discover and count all the possiblilties 
-        int res = 0;
-        for(int i = 0; i < 10; i++) {
-            res = (res + solverrr(i, positions, n, store)) % 1000000007;
-        }
-        
-        return res;
+        vector<vector<int>> dp(10, vector<int> (n + 1, -1));
+        int ans = 0;
+        for (int d = 0; d < 10; d++) 
+            (ans += solve(d, n - 1, dp)) %= mod;
+        return ans;
     }
 };
