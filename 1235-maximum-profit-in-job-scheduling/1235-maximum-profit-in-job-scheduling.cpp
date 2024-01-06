ab22a1d03194c3825aa067e0edd29b64f35faa14
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2024-01-06 17:16:13 +0530

    Time: 167 ms (70.02%), Space: 77.2 MB (54.67%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,10 +1,9 @@
 class Solution {
 private:
-    int findNextIndex(int i, vector<vector<int>>& jobs) {
-        int low = i + 1;
-        int high = jobs.size() - 1;
+    int findNextIdx(int i, int N, vector<vector<int>>& jobs) {
+        int low = i + 1, high = N - 1;
         while (low <= high) {
-            int mid = (low + high) / 2;
+            int mid = (low + high) >> 1;
             if (jobs[i][1] <= jobs[mid][0]) {
                 high = mid - 1;
             } else {
@@ -13,23 +12,28 @@ private:
         }
         return low;
     }
-    int solve(int i, vector<vector<int>>& jobs, vector<int>& dp) {
-        if (i >= jobs.size()) return 0;
-        if (dp[i] != -1) return dp[i];
-        int notPick = solve(i + 1, jobs, dp);
-        int j = findNextIndex(i, jobs);
-        int pick = jobs[i][2] + solve(j, jobs, dp);
-        return dp[i] = max(pick, notPick);
+    int solve(int i, int N, vector<vector<int>>& jobs, vector<int>& dp) {
+        if (i == N) return 0;
+        int& ans = dp[i];
+        if (ans != -1) return ans;
+        ans = solve(i + 1, N, jobs, dp);
+        int nextIdx = findNextIdx(i, N, jobs);
+        ans = max(ans, jobs[i][2] + solve(nextIdx, N, jobs, dp));
+        return ans;
     }
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        vector<vector<int>> jobs;
-        for (int i = 0; i < startTime.size(); i++) {
-            jobs.push_back({startTime[i], endTime[i], profit[i]});
+        int N = profit.size();
+        vector<vector<int>> jobs(N, vector<int> (3));
+        for (int i = 0; i < N; i++) {
+            jobs[i][0] = startTime[i];
+            jobs[i][1] = endTime[i];
+            jobs[i][2] = profit[i];
         }
-        sort(begin(jobs),end(jobs));
-        vector<int> dp(startTime.size(), -1);
-        int ans = solve(0, jobs, dp);
-        return ans;
+        sort(begin(jobs), end(jobs), [&](auto &a, auto &b) {
+            return a[0] < b[0];
+        });
+        vector<int> dp(N, -1);
+        return solve(0, N, jobs, dp);
     }
 };
