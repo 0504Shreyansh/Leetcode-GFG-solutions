cf20477d8c39f3d441eedf8668c2c450766aacbc
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2024-01-06 17:18:20 +0530

    Time: 133 ms (78.80%), Space: 70.9 MB (71.88%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,18 +1,14 @@
 class Solution {
 private:
-    int findNextIdx(int i, int N, vector<vector<int>>& jobs) {
+    int findNextIdx(int i, int N, vector<array<int, 3>>& jobs) {
         int low = i + 1, high = N - 1;
         while (low <= high) {
             int mid = (low + high) >> 1;
-            if (jobs[i][1] <= jobs[mid][0]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
+            (jobs[i][1] <= jobs[mid][0]) ? high = mid - 1 : low = mid + 1;
         }
         return low;
     }
-    int solve(int i, int N, vector<vector<int>>& jobs, vector<int>& dp) {
+    int solve(int i, int N, vector<array<int, 3>>& jobs, vector<int>& dp) {
         if (i == N) return 0;
         int& ans = dp[i];
         if (ans != -1) return ans;
@@ -24,15 +20,11 @@ private:
 public:
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int N = profit.size();
-        vector<vector<int>> jobs(N, vector<int> (3));
+        vector<array<int, 3>> jobs;
         for (int i = 0; i < N; i++) {
-            jobs[i][0] = startTime[i];
-            jobs[i][1] = endTime[i];
-            jobs[i][2] = profit[i];
+            jobs.push_back({startTime[i], endTime[i], profit[i]});
         }
-        sort(begin(jobs), end(jobs), [&](auto &a, auto &b) {
-            return a[0] < b[0];
-        });
+        sort(begin(jobs), end(jobs));
         vector<int> dp(N, -1);
         return solve(0, N, jobs, dp);
     }
