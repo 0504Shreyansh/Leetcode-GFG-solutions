0e158672f6774719e3ddaec6979f2d745bcd0cfb
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2024-01-19 06:17:28 +0530

    Time: 16 ms (31.12%), Space: 11.1 MB (35.74%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
-    /* METHOD 1 : RECURSION
-    int solve(int i, int j, vector<vector<int>>& matrix) {
-        if(i == matrix.size()-1)
-            return matrix[i][j];
-        int down = solve(i+1, j, matrix);
-        int leftDiagonal = 1e9, rightDiagonal = 1e9;
-        if(j > 0) leftDiagonal = solve(i+1, j-1, matrix);
-        if(j < matrix.size()-1) rightDiagonal = solve(i+1, j+1, matrix);
-        return dp[i][j] = matrix[i][j] + min({down, leftDiagonal, rightDiagonal});
-    }*/
-    /* METHOD 2 : MEMOIZATION
-    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
-        if(i == matrix.size()-1)
-            return matrix[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = solve(i+1, j, matrix, dp);
-        int leftDiagonal = 1e9, rightDiagonal = 1e9;
-        if(j > 0) leftDiagonal = solve(i+1, j-1, matrix, dp);
-        if(j < matrix.size()-1) rightDiagonal = solve(i+1, j+1, matrix, dp);
-        return dp[i][j] = matrix[i][j] + min({down, leftDiagonal, rightDiagonal});
-    }*/
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        // Space optimisation
-        int N = matrix.size();
-        vector<int> prev(matrix[0]), cur(N);
-        for(int i = 1; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                int down = prev[j];
-                int leftDiagonal = 1e9, rightDiagonal = 1e9;
-                if(j > 0) leftDiagonal = prev[j-1];
-                if(j < N-1) rightDiagonal = prev[j+1];
-                cur[j] = matrix[i][j] + min({down, leftDiagonal, rightDiagonal});
+        int n = matrix.size(), m = matrix[0].size();
+        vector<int> prev(matrix[0]);
+        for (int i = 1; i < n; i++) {
+            vector<int> curr(m);
+            for (int j = 0; j < m; j++) {
+                curr[j] = matrix[i][j] + min({prev[j], (j != 0 ? prev[j - 1] : 10001), (j + 1 < m ? prev[j + 1] : 10001)});
             }
-            prev = cur;
+            prev = curr;
         }
-
-        return *min_element(begin(prev),end(prev));   
+        return *min_element(begin(prev), end(prev));
     }
 };
