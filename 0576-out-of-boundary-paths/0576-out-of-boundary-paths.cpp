ff3fe611a493188ded293b07e024b95554e5c2bc
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2024-01-26 15:02:48 +0530

    Time: 3 ms (89.56%), Space: 10.6 MB (23.29%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -1,37 +1,20 @@
-#define mod 1000000007
 class Solution {
-public:
-    vector<pair<int,int>> direction = {{-1,0},{1,0},{0,1},{0,-1}};
-    int dfs(int n, int m, int maxMoves, int i, int j, vector<vector<vector<int>>>& dp) {
-
-        // Count it -> out of boundary
-        if (i<0 || i>=n || j<0 || j>=m) {
-            return 1;
+private:
+    int dirs[5] = {0, -1, 0, 1, 0};
+    const int mod = 1e9 + 7;
+    int solve(int row, int col, int moves, int m, int n, vector<vector<vector<int>>>& dp) {
+        if (row < 0 || row >= m || col < 0 || col >= n) return 1;
+        if (moves == 0) return 0;
+        int& ans = dp[row][col][moves], res = 0;
+        if (ans != -1) return ans;
+        for (int k = 0; k < 4; k++) {
+            res = (res + solve(row + dirs[k], col + dirs[k + 1], moves - 1, m, n, dp)) % mod;
         }
-
-        // All moves exhausted
-        if (maxMoves==0) {
-            return 0;
-        }
-
-        // Already calculated
-        // dp[i][j][k] -> Standing at (i, j) with k more moves left
-        if (dp[i][j][maxMoves] != -1) {
-            return dp[i][j][maxMoves];
-        }
-
-        int cur = 0;
-        for (auto [x, y] : direction) {
-            int X = i + x;
-            int Y = j + y;
-            cur = (cur + dfs(n, m, maxMoves-1, X, Y, dp)) % mod;
-        }
-        return dp[i][j][maxMoves] = cur % mod;
+        return ans = res;
     }
+public:
     int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
-        vector<vector<vector<int>>> dp(m, 
-            vector<vector<int>> (n, vector<int> (maxMove + 1, -1)));
-        int totPaths = dfs(m, n, maxMove, startRow, startColumn, dp);
-        return totPaths;
+        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int> (maxMove + 1, -1)));
+        return solve(startRow, startColumn, maxMove, m, n, dp);
     }
 };
