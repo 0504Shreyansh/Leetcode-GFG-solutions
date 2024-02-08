126d5be1c8678b4427569f3ae877ef7884168d7a
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2024-02-08 06:15:29 +0530

    Time: 916 ms (5.01%), Space: 50.8 MB (7.90%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
     int numSquares(int n) {
-        
-        vector<int> visited(1e4), perf;
-        queue<int> q;
-        q.push(n);
-        int count = 1, i = 1;
-        
-        while(i*i<=n) 
-            perf.push_back(i*i), i++;
-        
-        while (q.size()) {
-            int sz = q.size();
-            for (int i = 0; i < sz; i++) {
-                int curr = q.front();
-                q.pop();
-                for (int k = 0; k < perf.size(); k++) {
-                    int val = 0;
-                    if(curr > perf[k]) {
-                        q.push(curr-perf[k]);
-                        visited[perf[k]] = 1;
-                    }
-                    else if(curr==perf[k])
-                        return count;
-                    else break;
+        queue<array<int, 2>> q({{n, 0}});
+        set<int> visit;
+        visit.insert(n);
+        while (!q.empty()) {
+            array<int, 2> a = q.front();
+            q.pop();
+            if (a[0] == 0) return a[1];
+            for (int i = 1; i * i <= a[0]; ++i) {
+                if (visit.find(a[0] - i * i) == visit.end()) {
+                    visit.insert(a[0] - i * i);
+                    q.push({a[0] - i * i, a[1] + 1});
                 }
-            } 
-            count++;
+            }
         }
-        
-        return 3;
+        return -1;
     }
 };
