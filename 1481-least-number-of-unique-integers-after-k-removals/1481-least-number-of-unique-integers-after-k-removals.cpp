7a6a58f589cd5f4669e213dd5968ecd0aed5907d
Shreyansh Jain <113186607+0504Shreyansh@users.noreply.github.com>
2024-02-16 06:13:26 +0530

    Time: 184 ms (40.44%), Space: 67.7 MB (37.54%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        
-        map<int,int> mp;
-        for(auto i:arr)
-            mp[i]++;
-        //greedily create minHeap so that minCount is at the top
+        map<int, int> freq;
+        for (auto &x : arr)
+            freq[x]++;
         priority_queue<int> pq;
-        for(auto i:mp)
-            pq.push(-i.second);
-        while(pq.size()) {
-            if(k >= -pq.top()) {
-                k += pq.top();
-                pq.pop();
-            }
-            else break;
+        for (auto &x : freq)
+            pq.push(-x.second);
+        while (!pq.empty() && k >= -pq.top()) {
+            k += pq.top();
+            pq.pop();
         }
-        
         return pq.size();
     }
 };
